867a31740023ed6189279a30d5c0dc0149bdc980
Jeff Hammond <jeff.science@gmail.com>
2015-10-23 18:27:52 +0000

    support MPI-3 RMA features via MPI-2 fallback if possible

--- include/par-res-kern_mpi.h
+++ include/par-res-kern_mpi.h
@@ -41,5 +41,51 @@ POSSIBILITY OF SUCH DAMAGE.
                 ( level==MPI_THREAD_FUNNELED ? "THREAD_FUNNELED" : \
                     ( level==MPI_THREAD_SINGLE ? "THREAD_SINGLE" : "THREAD_UNKNOWN" ) ) ) )
 
+/* We should set an attribute that indicates we need to free memory
+ * when using this so that the MPI_Win_free does not create a
+ * double-free situation when paired with a real MPI_Win_create call. */
+int PRK_Win_allocate(MPI_Aint size, int disp_unit, MPI_Info info, 
+                     MPI_Comm comm, void * baseptr, MPI_Win * win)
+{
+#if MPI_VERSION < 3 
+    int rc = MPI_SUCCESS;
+    MPI_Info alloc_info = MPI_INFO_NULL;
+    MPI_Info win_info = MPI_INFO_NULL;
+    rc = MPI_Alloc_mem(size, alloc_info, &baseptr);
+    if (rc!=MPI_SUCCESS) MPI_Abort(rc,MPI_COMM_WORLD);
+    rc = MPI_Win_create(baseptr, size, disp_unit, win_info, comm, win);
+    if (rc!=MPI_SUCCESS) MPI_Abort(rc,MPI_COMM_WORLD);
+    return MPI_SUCCESS;
+#else
+    return MPI_Win_allocate(size, disp_unit, info, comm, baseptr, win);
+#endif
+}
+
+int PRK_Win_free(MPI_Win * win)
+{
+#if MPI_VERSION < 3 
+    int rc = MPI_SUCCESS;
+    int flag = 0;
+    void * attr_ptr;
+    rc = MPI_Win_get_attr(*win, MPI_WIN_BASE, (void*)&attr_ptr, &flag);
+    if (rc!=MPI_SUCCESS) MPI_Abort(rc,MPI_COMM_WORLD);
+    /* We do not check for the case of size=0 here,
+     * but it may be worth adding in the future. */
+    if (flag) {
+        void * baseptr = (void*)attr_ptr;
+        rc = MPI_Free_mem(baseptr);
+        if (rc!=MPI_SUCCESS) MPI_Abort(rc,MPI_COMM_WORLD);
+    } else {
+        int rank;
+        MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+        printf("%d: could not capture baseptr from win attribute: memory leak.\n",rank);
+    }
+    rc = MPI_Win_free(win);
+    if (rc!=MPI_SUCCESS) MPI_Abort(rc,MPI_COMM_WORLD);
+    return MPI_SUCCESS;
+#else
+    return MPI_Win_free(win);
+#endif
+}
 
 extern void bail_out(int);
