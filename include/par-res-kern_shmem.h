67b9c1a8c44cd28b9366750f89f542eed516229e
Jeff Hammond <jeff.science@gmail.com>
2015-12-22 07:28:51 -0800

    redo preprocessor logic per Jim's comments

--- include/par-res-kern_shmem.h
+++ include/par-res-kern_shmem.h
@@ -34,13 +34,14 @@ POSSIBILITY OF SUCH DAMAGE.
 
 extern void bail_out(int);
 
-/* Handle functions first and preprocessor symbols second,  *
- * i.e. separately, to keep the code mildly readable and    *
- * avoid duplicating any of the function signatures, which  *
- * could lead to errors.                                    */
+#if defined(SHMEM_MAJOR_VERSION) && defined(SHMEM_MINOR_VERSION) && \\
+       ((SHMEM_MAJOR_VERSION>1) || \\
+        ((SHMEM_MAJOR_VERSION == 1) && (SHMEM_MINOR_VERSION >= 2)))
+#define PRK_HAVE_OPENSHMEM_1_2
+#endif
 
 static void prk_shmem_init(void) {
-#if (SHMEM_MAJOR_VERSION>1) || ((SHMEM_MAJOR_VERSION == 1) && (SHMEM_MINOR_VERSION >= 2))
+#ifdef PRK_HAVE_OPENSHMEM_1_2
     shmem_init();
 #else
     start_pes(0);
@@ -48,7 +49,7 @@ static void prk_shmem_init(void) {
 }
 
 static void prk_shmem_finalize(void) {
-#if (SHMEM_MAJOR_VERSION>1) || ((SHMEM_MAJOR_VERSION == 1) && (SHMEM_MINOR_VERSION >= 2))
+#ifdef PRK_HAVE_OPENSHMEM_1_2
     shmem_finalize();
 #else
     /* shmem_finalize contains implicit barrier, so we include this
@@ -58,7 +59,7 @@ static void prk_shmem_finalize(void) {
 }
 
 static int prk_shmem_my_pe(void) {
-#if (SHMEM_MAJOR_VERSION>1) || ((SHMEM_MAJOR_VERSION == 1) && (SHMEM_MINOR_VERSION >= 2))
+#ifdef PRK_HAVE_OPENSHMEM_1_2
     return shmem_my_pe();
 #else
     return _my_pe();
@@ -66,7 +67,7 @@ static int prk_shmem_my_pe(void) {
 }
 
 static int prk_shmem_n_pes(void) {
-#if (SHMEM_MAJOR_VERSION>1) || ((SHMEM_MAJOR_VERSION == 1) && (SHMEM_MINOR_VERSION >= 2))
+#ifdef PRK_HAVE_OPENSHMEM_1_2
     return shmem_my_pe();
 #else
     return _my_pe();
@@ -74,7 +75,7 @@ static int prk_shmem_n_pes(void) {
 }
 
 static void * prk_shmem_malloc(size_t n) {
-#if (SHMEM_MAJOR_VERSION>1) || ((SHMEM_MAJOR_VERSION == 1) && (SHMEM_MINOR_VERSION >= 2))
+#ifdef PRK_HAVE_OPENSHMEM_1_2
     return shmem_malloc(n);
 #else
     return shmalloc(n);
@@ -82,7 +83,7 @@ static void * prk_shmem_malloc(size_t n) {
 }
 
 static void * prk_shmem_realloc(void *ptr, size_t size) {
-#if (SHMEM_MAJOR_VERSION>1) || ((SHMEM_MAJOR_VERSION == 1) && (SHMEM_MINOR_VERSION >= 2))
+#ifdef PRK_HAVE_OPENSHMEM_1_2
     return shmem_realloc(ptr,size);
 #else
     return shrealloc(ptr,size);
@@ -90,7 +91,7 @@ static void * prk_shmem_realloc(void *ptr, size_t size) {
 }
 
 static void * prk_shmem_align(size_t alignment, size_t size) {
-#if (SHMEM_MAJOR_VERSION>1) || ((SHMEM_MAJOR_VERSION == 1) && (SHMEM_MINOR_VERSION >= 2))
+#ifdef PRK_HAVE_OPENSHMEM_1_2
     return shmem_align(alignment,size);
 #else
     return shmalign(alignment,size);
@@ -98,64 +99,23 @@ static void * prk_shmem_align(size_t alignment, size_t size) {
 }
 
 static void prk_shmem_free(void * ptr) {
-#if (SHMEM_MAJOR_VERSION>1) || ((SHMEM_MAJOR_VERSION == 1) && (SHMEM_MINOR_VERSION >= 2))
+#ifdef PRK_HAVE_OPENSHMEM_1_2
     shmem_free(ptr);
 #else
     shfree(ptr);
 #endif
 }
 
-#if defined(SHMEM_MAJOR_VERSION) && defined(SHMEM_MINOR_VERSION)
-/* OpenSHMEM 1.3 and later */
-# if (SHMEM_MAJOR_VERSION>1) || ((SHMEM_MAJOR_VERSION == 1) && (SHMEM_MINOR_VERSION >= 2))
+/* The SHMEM_* constants were added in OpenSHMEM 1.2
+ * and the _SHMEM* constants were deprecated in OpenSHMEM 1.3. */
+#ifdef PRK_HAVE_OPENSHMEM_1_2
 #  define PRK_SHMEM_BCAST_SYNC_SIZE SHMEM_BCAST_SYNC_SIZE
 #  define PRK_SHMEM_REDUCE_SYNC_SIZE SHMEM_REDUCE_SYNC_SIZE
 #  define PRK_SHMEM_SYNC_VALUE SHMEM_SYNC_VALUE
 #  define PRK_SHMEM_REDUCE_MIN_WRKDATA_SIZE SHMEM_REDUCE_MIN_WRKDATA_SIZE
-/* OpenSHMEM 1.2 and earlier, or other SHMEM that defines VERSION symbols */
-# else
-#  define PRK_SHMEM_BCAST_SYNC_SIZE _SHMEM_BCAST_SYNC_SIZE
-#  define PRK_SHMEM_REDUCE_SYNC_SIZE _SHMEM_REDUCE_SYNC_SIZE
-#  define PRK_SHMEM_SYNC_VALUE _SHMEM_SYNC_VALUE
-#  define PRK_SHMEM_REDUCE_MIN_WRKDATA_SIZE _SHMEM_REDUCE_MIN_WRKDATA_SIZE
-# endif
-/* no VERSION symbols */
 #else
-/* PRK_SHMEM_BCAST_SYNC_SIZE */
-# if defined(SHMEM_BCAST_SYNC_SIZE)
-#  define PRK_SHMEM_BCAST_SYNC_SIZE SHMEM_BCAST_SYNC_SIZE
-# elif defined(_SHMEM_BCAST_SYNC_SIZE)
 #  define PRK_SHMEM_BCAST_SYNC_SIZE _SHMEM_BCAST_SYNC_SIZE
-# else
-#  error No preprocesor definition of SHMEM_BCAST_SYNC_SIZE!
-# endif
-/* PRK_SHMEM_REDUCE_SYNC_SIZE */
-# if defined(SHMEM_REDUCE_SYNC_SIZE)
-#  define PRK_SHMEM_REDUCE_SYNC_SIZE SHMEM_REDUCE_SYNC_SIZE
-# elif defined(_SHMEM_REDUCE_SYNC_SIZE)
 #  define PRK_SHMEM_REDUCE_SYNC_SIZE _SHMEM_REDUCE_SYNC_SIZE
-# else
-#  error No preprocesor definition of SHMEM_REDUCE_SYNC_SIZE!
-# endif
-/* PRK_SHMEM_SYNC_VALUE */
-# if defined(SHMEM_SYNC_VALUE)
-#  define PRK_SHMEM_SYNC_VALUE SHMEM_SYNC_VALUE
-# elif defined(SHMEM_SYNC_VALUE)
 #  define PRK_SHMEM_SYNC_VALUE _SHMEM_SYNC_VALUE
-# else
-#  error No preprocesor definition of SHMEM_SYNC_VALUE!
-# endif
-/* PRK_SHMEM_REDUCE_MIN_WRKDATA_SIZE */
-# if defined(SHMEM_REDUCE_MIN_WRKDATA_SIZE)
-#  define PRK_SHMEM_REDUCE_MIN_WRKDATA_SIZE SHMEM_REDUCE_MIN_WRKDATA_SIZE
-# elif defined(SHMEM_REDUCE_MIN_WRKDATA_SIZE)
 #  define PRK_SHMEM_REDUCE_MIN_WRKDATA_SIZE _SHMEM_REDUCE_MIN_WRKDATA_SIZE
-# else
-#  error No preprocesor definition of SHMEM_REDUCE_MIN_WRKDATA_SIZE!
-# endif
-/* end manual definition detection */
-#endif // VERSION symbols
-
-
-
-
+#endif
