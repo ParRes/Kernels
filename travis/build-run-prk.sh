0d316d6ace9e58773fd7e14d255e856dc925c752
Jeff Hammond <jeff.science@gmail.com>
2015-12-24 15:19:30 -0800

    actually run MPI1 tests

--- travis/build-run-prk.sh
+++ travis/build-run-prk.sh
@@ -28,7 +28,7 @@ case "$PRK_TARGET" in
         ./OPENMP/Stencil/stencil $OMP_NUM_THREADS 10 1024
         ./OPENMP/Transpose/transpose $OMP_NUM_THREADS 10 1024 32
         # less support
-        ./OPENMP/Reduce/reduce $OMP_NUM_THREADS 10 1024
+        ./OPENMP/Reduce/reduce $OMP_NUM_THREADS 10 16777216
         ./OPENMP/Nstream/nstream $OMP_NUM_THREADS 10 16777216 32
         ./OPENMP/Sparse/sparse $OMP_NUM_THREADS 10 10 5
         ./OPENMP/DGEMM/dgemm $OMP_NUM_THREADS 10 1024 32
@@ -38,5 +38,22 @@ case "$PRK_TARGET" in
         ./OPENMP/Synch_global/global $OMP_NUM_THREADS 10 16384
         ./OPENMP/RefCount_private/private $OMP_NUM_THREADS 16777216
         ./OPENMP/RefCount_shared/shared $OMP_NUM_THREADS 16777216 1024
+        ;;
+    allmpi1)
+        echo "MPI-1"
+        make $PRK_TARGET
+        export PRK_MPI_PROCS
+        # widely supported
+        mpirun -n $PRK_MPI_PROCS ./MPI1/Synch_p2p/p2p 10 1024 1024
+        mpirun -n $PRK_MPI_PROCS ./MPI1/Stencil/stencil 10 1024
+        mpirun -n $PRK_MPI_PROCS ./MPI1/Transpose/transpose 10 1024 32
+        # less support
+        mpirun -n $PRK_MPI_PROCS ./MPI1/Reduce/reduce 10 16777216
+        mpirun -n $PRK_MPI_PROCS ./MPI1/Nstream/nstream 10 16777216 32
+        mpirun -n $PRK_MPI_PROCS ./MPI1/Sparse/sparse 10 10 5
+        mpirun -n $PRK_MPI_PROCS ./MPI1/DGEMM/dgemm 10 1024 32
+        mpirun -n $PRK_MPI_PROCS ./MPI1/Random/random 32 20
+        # no serial equivalent
+        mpirun -n $PRK_MPI_PROCS ./MPI1/Synch_global/global 10 16384
         ;;
 esac
