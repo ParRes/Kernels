558f7c79bac9d7136af0dce57c7e60de30a4184e
Jeff Hammond <jeff.science@gmail.com>
2015-12-24 08:02:01 -0800

    run tests for serial and OpenMP

--- travis/build-run-prk.sh
+++ travis/build-run-prk.sh
@@ -7,10 +7,24 @@ case "$PRK_TARGET" in
     allserial)
         echo "Serial"
         make $PRK_TARGET
+        # widely supported
+        ./SERIAL/Synch_p2p/p2p 10 1024 1024
+        ./SERIAL/Stencil/stencil 10 1024 32
+        ./SERIAL/Transpose/transpose 10 1024 32
+        # less support
+        ./SERIAL/Reduce/reduce 10 1024
+        ./SERIAL/Random/random 64 10 16384
+        ./SERIAL/Nstream/nstream 10 16777216 32
+        ./SERIAL/Sparse/sparse 10 10 5
+        ./SERIAL/DGEMM/dgemm 10 1024 32
         ;;
 
     allopenmp)
         echo "OpenMP"
         make $PRK_TARGET
+        export OMP_NUM_THREADS=4
+        ./SERIAL/Synch_p2p/p2p 10 1024 1024
+        ./SERIAL/Stencil/stencil 10 1024 32
+        ./SERIAL/Transpose/transpose 10 1024 32
         ;;
 esac
