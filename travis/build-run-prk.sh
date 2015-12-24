4b4f12d9c34b5b813ce8725155a255bcb641a9a8
Jeff Hammond <jeff.science@gmail.com>
2015-12-24 15:32:44 -0800

    fix mpirun issue and add other MPI tests

--- travis/build-run-prk.sh
+++ travis/build-run-prk.sh
@@ -7,53 +7,89 @@ case "$PRK_TARGET" in
     allserial)
         echo "Serial"
         make $PRK_TARGET
+        export PRK_TARGET_PATH=SERIAL
         # widely supported
-        ./SERIAL/Synch_p2p/p2p 10 1024 1024
-        ./SERIAL/Stencil/stencil 10 1024 32
-        ./SERIAL/Transpose/transpose 10 1024 32
+        $PRK_TARGET_PATH/Synch_p2p/p2p 10 1024 1024
+        $PRK_TARGET_PATH/Stencil/stencil 10 1024 32
+        $PRK_TARGET_PATH/Transpose/transpose 10 1024 32
         # less support
-        ./SERIAL/Reduce/reduce 10 1024
-        ./SERIAL/Random/random 64 10 16384
-        ./SERIAL/Nstream/nstream 10 16777216 32
-        ./SERIAL/Sparse/sparse 10 10 5
-        ./SERIAL/DGEMM/dgemm 10 1024 32
+        $PRK_TARGET_PATH/Reduce/reduce 10 1024
+        $PRK_TARGET_PATH/Random/random 64 10 16384
+        $PRK_TARGET_PATH/Nstream/nstream 10 16777216 32
+        $PRK_TARGET_PATH/Sparse/sparse 10 10 5
+        $PRK_TARGET_PATH/DGEMM/dgemm 10 1024 32
         ;;
 
     allopenmp)
         echo "OpenMP"
         make $PRK_TARGET
+        export PRK_TARGET_PATH=OPENMP
         export OMP_NUM_THREADS=4
         # widely supported
-        ./OPENMP/Synch_p2p/p2p $OMP_NUM_THREADS 10 1024 1024
-        ./OPENMP/Stencil/stencil $OMP_NUM_THREADS 10 1024
-        ./OPENMP/Transpose/transpose $OMP_NUM_THREADS 10 1024 32
+        $PRK_TARGET_PATH/Synch_p2p/p2p $OMP_NUM_THREADS 10 1024 1024
+        $PRK_TARGET_PATH/Stencil/stencil $OMP_NUM_THREADS 10 1024
+        $PRK_TARGET_PATH/Transpose/transpose $OMP_NUM_THREADS 10 1024 32
         # less support
-        ./OPENMP/Reduce/reduce $OMP_NUM_THREADS 10 16777216
-        ./OPENMP/Nstream/nstream $OMP_NUM_THREADS 10 16777216 32
-        ./OPENMP/Sparse/sparse $OMP_NUM_THREADS 10 10 5
-        ./OPENMP/DGEMM/dgemm $OMP_NUM_THREADS 10 1024 32
+        $PRK_TARGET_PATH/Reduce/reduce $OMP_NUM_THREADS 10 16777216
+        $PRK_TARGET_PATH/Nstream/nstream $OMP_NUM_THREADS 10 16777216 32
+        $PRK_TARGET_PATH/Sparse/sparse $OMP_NUM_THREADS 10 10 5
+        $PRK_TARGET_PATH/DGEMM/dgemm $OMP_NUM_THREADS 10 1024 32
         # random is broken right now it seems
-        #./OPENMP/Random/random $OMP_NUM_THREADS 10 16384 32
+        #$PRK_TARGET_PATH/Random/random $OMP_NUM_THREADS 10 16384 32
         # no serial equivalent
-        ./OPENMP/Synch_global/global $OMP_NUM_THREADS 10 16384
-        ./OPENMP/RefCount_private/private $OMP_NUM_THREADS 16777216
-        ./OPENMP/RefCount_shared/shared $OMP_NUM_THREADS 16777216 1024
+        $PRK_TARGET_PATH/Synch_global/global $OMP_NUM_THREADS 10 16384
+        $PRK_TARGET_PATH/RefCount_private/private $OMP_NUM_THREADS 16777216
+        $PRK_TARGET_PATH/RefCount_shared/shared $OMP_NUM_THREADS 16777216 1024
         ;;
     allmpi1)
         echo "MPI-1"
         make $PRK_TARGET
-        export PRK_MPI_PROCS
+        export PRK_TARGET_PATH=MPI1
+        export PRK_MPI_PROCS=4
         # widely supported
-        mpirun -n $PRK_MPI_PROCS ./MPI1/Synch_p2p/p2p 10 1024 1024
-        mpirun -n $PRK_MPI_PROCS ./MPI1/Stencil/stencil 10 1024
-        mpirun -n $PRK_MPI_PROCS ./MPI1/Transpose/transpose 10 1024 32
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Synch_p2p/p2p 10 1024 1024
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Stencil/stencil 10 1024
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Transpose/transpose 10 1024 32
         # less support
-        mpirun -n $PRK_MPI_PROCS ./MPI1/Reduce/reduce 10 16777216
-        mpirun -n $PRK_MPI_PROCS ./MPI1/Nstream/nstream 10 16777216 32
-        mpirun -n $PRK_MPI_PROCS ./MPI1/Sparse/sparse 10 10 5
-        mpirun -n $PRK_MPI_PROCS ./MPI1/DGEMM/dgemm 10 1024 32
-        mpirun -n $PRK_MPI_PROCS ./MPI1/Random/random 32 20
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Reduce/reduce 10 16777216
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Nstream/nstream 10 16777216 32
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Sparse/sparse 10 10 5
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/DGEMM/dgemm 10 1024 32
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Random/random 32 20
         # no serial equivalent
-        mpirun -n $PRK_MPI_PROCS ./MPI1/Synch_global/global 10 16384
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Synch_global/global 10 16384
+        ;;
+    allmpiomp)
+        echo "MPI+OpenMP"
+        make $PRK_TARGET
+        export PRK_TARGET_PATH=MPIOMP
+        export PRK_MPI_PROCS=4
+        # widely supported
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Synch_p2p/p2p 10 1024 1024
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Stencil/stencil 10 1024
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Transpose/transpose 10 1024 32
+        # less support
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Nstream/nstream 10 16777216 32
+        ;;
+    allmpirma)
+        echo "MPI-RMA"
+        make $PRK_TARGET
+        export PRK_TARGET_PATH=MPIRMA
+        export PRK_MPI_PROCS=4
+        # widely supported
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Synch_p2p/p2p 10 1024 1024
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Stencil/stencil 10 1024
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Transpose/transpose 10 1024 32
+        ;;
+    allmpishm)
+        echo "MPI+MPI"
+        make $PRK_TARGET
+        export PRK_TARGET_PATH=MPISHM
+        export PRK_MPI_PROCS=4
+        export PRK_MPISHM_RANKS=$(($PRK_MPI_PROCS/2))
+        # widely supported
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Synch_p2p/p2p $PRK_MPISHM_RANKS 10 1024 1024
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Stencil/stencil $PRK_MPISHM_RANKS 10 1024
+        mpirun -n $PRK_MPI_PROCS $PRK_TARGET_PATH/Transpose/transpose $PRK_MPISHM_RANKS 10 1024 32
         ;;
 esac
