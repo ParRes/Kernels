2dfd1f2620871f56dc330da36f6dc2fc7f659604
Jeff Hammond <jeff.science@gmail.com>
2015-12-25 14:14:51 -0800

    use apt-get -y

--- travis/install-compilers.sh
+++ travis/install-compilers.sh
@@ -20,18 +20,18 @@ case "$os" in
         sudo apt-get update -q
         case "$MPI_IMPL" in
             mpich1)
-                sudo apt-get install -q cmake gfortran mpich-shmem-bin libmpich-shmem1.0-dev
+                sudo apt-get install -y cmake gfortran mpich-shmem-bin libmpich-shmem1.0-dev
                 ;;
             mpich2)
-                sudo apt-get install -q cmake gfortran mpich2 libmpich2-3 libmpich2-dev
+                sudo apt-get install -y cmake gfortran mpich2 libmpich2-3 libmpich2-dev
                 ;;
             mpich|mpich3)
-                sudo apt-get install -q cmake gfortran libcr0 default-jdk
+                sudo apt-get install -y cmake gfortran libcr0 default-jdk
                 wget -q http://www.cebacad.net/files/mpich/ubuntu/mpich-3.2b3/mpich_3.2b3-1ubuntu_amd64.deb
                 sudo dpkg -i ./mpich_3.2b3-1ubuntu_amd64.deb
                 ;;
             openmpi)
-                sudo apt-get install -q cmake gfortran openmpi-bin openmpi-common libopenmpi-dev
+                sudo apt-get install -y cmake gfortran openmpi-bin openmpi-common libopenmpi-dev
                 ;;
             *)
                 echo "Unknown MPI implementation: $MPI_IMPL"
--- travis/install-mpi.sh
+++ travis/install-mpi.sh
@@ -31,12 +31,12 @@ case "$os" in
         sudo apt-get update -q
         case "$MPI_IMPL" in
             mpich)
-                sudo apt-get install -q gfortran libcr0 default-jdk
+                sudo apt-get install -y gfortran libcr0 default-jdk
                 wget -q http://www.cebacad.net/files/mpich/ubuntu/mpich-3.2b3/mpich_3.2b3-1ubuntu_amd64.deb
                 sudo dpkg -i ./mpich_3.2b3-1ubuntu_amd64.deb
                 ;;
             openmpi)
-                sudo apt-get install -q cmake gfortran openmpi-bin openmpi-common libopenmpi-dev
+                sudo apt-get install -y cmake gfortran openmpi-bin openmpi-common libopenmpi-dev
                 ;;
             *)
                 echo "Unknown MPI implementation: $MPI_IMPL"
