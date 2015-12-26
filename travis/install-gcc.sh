5bbaec30a315b3757790f8f7daa18cec616f6100
Jeff Hammond <jeff.science@gmail.com>
2015-12-25 21:03:12 -0800

    build GCC 5.3 for Grappa

---
+++ travis/install-gcc.sh
@@ -0,0 +1,32 @@
+set -e
+set -x
+
+os=`uname`
+
+case "$os" in
+    Darwin)
+        echo "Mac"
+        brew update
+        # this is 5.3.0 or later
+        brew install gcc --without-multilib
+        ;;
+    Linux)
+        echo "Linux"
+        wget -q ftp://gcc.gnu.org/pub/gcc/releases/gcc-5.3.0/gcc-5.3.0.tar.bz2
+        tar -C /tmp -xjf gcc-5.3.0.tar.bz2
+        cd /tmp/gcc-5.3.0
+        ./contrib/download_prerequisites
+        mkdir build && cd build
+        ../configure \
+          --prefix=$HOME
+          --enable-threads=posix \
+          --with-system-zlib \
+          --enable-__cxa_atexit \
+          --enable-languages=c,c++ \
+          --with-tune=native \
+          --enable-lto \
+          --disable-multilib
+        make -j4 && make install
+        ;;
+esac
+
