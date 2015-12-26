05a51548e9b743a785583a8064068a4ee81eff0a
Jeff Hammond <jeff.science@gmail.com>
2015-12-25 16:32:18 -0800

    CMake ruins everything it touches

--- travis/install-grappa.sh
+++ travis/install-grappa.sh
@@ -20,6 +20,13 @@ case "$os" in
         sudo apt-get install -y gfortran libcr0 default-jdk
         wget -q http://www.cebacad.net/files/mpich/ubuntu/mpich-3.2b3/mpich_3.2b3-1ubuntu_amd64.deb
         sudo dpkg -i ./mpich_3.2b3-1ubuntu_amd64.deb
+        # I hate CMake so so much...
+        wget -q https://cmake.org/files/v3.4/cmake-3.4.1.tar.gz
+        tar -C $HOME -xzf cmake-3.4.1.tar.gz
+        cd ~/cmake-3.4.1
+        mkdir build && cd build
+        cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME
+        make -j4 && make install
         ;;
 esac
 
