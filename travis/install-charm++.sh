0f03d067e4b9e4f4dc538c710c6bc1021b3af5b1
Jeff Hammond <jeff.science@gmail.com>
2015-12-26 19:44:39 -0800

    try netlrts smp for Charm++ instead of multicore-linux64 [ci skip]

--- travis/install-charm++.sh
+++ travis/install-charm++.sh
@@ -26,8 +26,8 @@ if [ ! -d "$TRAVIS_ROOT/charm" ]; then
             cd charm-6.7.0
             # This fails with: The authenticity of host 'localhost (127.0.0.1)' can't be established.
             #./build $RUNTIME netlrts-linux-x86_64 --with-production -j4
-            #./build $RUNTIME netlrts-linux-x86_64 smp --with-production
-            ./build $RUNTIME multicore-linux64 --with-production
+            ./build $RUNTIME netlrts-linux-x86_64 smp --with-production
+            #./build $RUNTIME multicore-linux64 --with-production
             ;;
     esac
 else
