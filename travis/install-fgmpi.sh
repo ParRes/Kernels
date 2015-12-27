736645a3ea5a8d6e50d0e68b28b38feb8fe86c4d
Jeff Hammond <jeff.science@gmail.com>
2015-12-26 16:44:04 -0800

    just remove sudo build now

--- travis/install-fgmpi.sh
+++ travis/install-fgmpi.sh
@@ -1,25 +1,9 @@
 set -e
 set -x
 
-os=`uname`
 TRAVIS_ROOT="$1"
 
 if [ ! -d "$TRAVIS_ROOT/fgmpi" ]; then
-    case "$os" in
-        Darwin)
-            #echo "Mac"
-            ;;
-
-        Linux)
-            #echo "Linux"
-            # cannot do this without sudo...
-            #sudo apt-get update -q
-            #sudo apt-get install -y gfortran libcr0 default-jdk
-            #wget -q http://www.cs.ubc.ca/~humaira/code/fgmpi_2.0-1_amd64.deb
-            #sudo dpkg -i ./fgmpi_2.0-1_amd64.deb
-            # for container builds
-            ;;
-    esac
     wget -q http://www.cs.ubc.ca/~humaira/code/fgmpi-2.0.tar.gz
     tar -C $TRAVIS_ROOT -xzf fgmpi-2.0.tar.gz
     cd $TRAVIS_ROOT/fgmpi-2.0
