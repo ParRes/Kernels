49fccacd2e894f1da1fffd5bb80aaecb0472c24a
Jeff Hammond <jeff.science@gmail.com>
2015-12-29 09:15:29 -0800

    add libfabric to shared lib path

--- travis/build-run-prk.sh
+++ travis/build-run-prk.sh
@@ -102,7 +102,7 @@ case "$PRK_TARGET" in
         echo "SHMEM"
         # BEGIN TEMPORARY FIX
         # This should be fixed by rpath (https://github.com/regrant/sandia-shmem/issues/83)
-        export LD_LIBRARY_PATH=$TRAVIS_ROOT/sandia-openshmem/lib:$LD_LIBRARY_PATH
+        export LD_LIBRARY_PATH=$TRAVIS_ROOT/sandia-openshmem/lib:$TRAVIS_ROOT/libfabric/lib:$LD_LIBRARY_PATH
         # END TEMPORARY FIX
         export SHMEM_ROOT=$TRAVIS_ROOT/sandia-openshmem
         echo "SHMEMTOP=$SHMEM_ROOT\nSHMEMCC=$SHMEM_ROOT/bin/oshcc" > common/make.defs
