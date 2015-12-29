47dab0b5b9484e228f48ff85044ee1619430e890
Jeff Hammond <jeff.science@gmail.com>
2015-12-29 15:33:36 -0800

    grab FGMPI from Github instead of UBC tar

--- travis/install-fgmpi.sh
+++ travis/install-fgmpi.sh
@@ -4,13 +4,27 @@ set -x
 TRAVIS_ROOT="$1"
 
 if [ ! -d "$TRAVIS_ROOT/fgmpi" ]; then
-    wget --no-check-certificate -q http://www.cs.ubc.ca/~humaira/code/fgmpi-2.0.tar.gz
-    tar -C $TRAVIS_ROOT -xzf fgmpi-2.0.tar.gz
-    cd $TRAVIS_ROOT/fgmpi-2.0
+
+    # TAR build
+    #wget --no-check-certificate -q http://www.cs.ubc.ca/~humaira/code/fgmpi-2.0.tar.gz
+    #tar -C $TRAVIS_ROOT -xzf fgmpi-2.0.tar.gz
+    #cd $TRAVIS_ROOT/fgmpi-2.0
+
+    # GIT build
+    cd $TRAVIS_ROOT
+    git clone --depth 10 https://github.com/humairakamal/fgmpi.git fgmpi-source
+    cd fgmpi-source
+
+    # TAR or GIT
     mkdir build && cd build
     # Clang defaults to C99, which chokes on "Set_PROC_NULL"
     ../configure --disable-fortran --disable-romio CFLAGS="-std=gnu89" --prefix=$TRAVIS_ROOT/fgmpi
-    make -j4 && make install
+    make -j2
+    make install
+
+    # Package install
+    # TODO (restore from older version but unpack in $TRAVIS_ROOT without sudo)
+
 else
     echo "FG-MPI installed..."
     find $TRAVIS_ROOT/fgmpi -name mpiexec
