972a4834a9cfc1a13baf99fe8a18a578aa592e49
Jeff Hammond <jeff.science@gmail.com>
2015-12-29 12:13:29 -0800

    stupid typo in Grappa CMake

--- travis/install-grappa.sh
+++ travis/install-grappa.sh
@@ -65,17 +65,16 @@ if [ ! -d "$TRAVIS_ROOT/grappa" ]; then
     #./configure --prefix=$TRAVIS_ROOT/grappa
     #cd build/Make+Release
     # Invoking CMake directly
-    export DGRAPPA_INSTALL_PREFIX=$TRAVIS_ROOT/grappa
     mkdir build && cd build
     export MPI_ROOT=$TRAVIS_ROOT/$MPI_IMPL
     cmake .. \
              -DCMAKE_CC_COMPILER="$PRK_CC" \
              -DCMAKE_CXX_COMPILER="$PRK_CXX" \
-             -DMPI_C_COMPILER="MPICH_CC=$PRK_CC $MPI_ROOT/bin/mpicc" \
-             -DMPI_C_LINK_FLAGS="-L$MPI_ROOT/lib"
+             -DMPI_C_COMPILER="\"MPICH_CC=$PRK_CC $MPI_ROOT/bin/mpicc\"" \
+             -DMPI_C_LINK_FLAGS="-L$MPI_ROOT/lib" \
              -DMPI_C_LIBRARIES="-lmpi" \
              -DMPI_C_INCLUDE_PATH="$MPI_ROOT/include" \
-             -DMPI_CXX_COMPILER="MPICH_CXX=$PRK_CXX $MPI_ROOT/bin/mpicxx" \
+             -DMPI_CXX_COMPILER="\"MPICH_CXX=$PRK_CXX $MPI_ROOT/bin/mpicxx\"" \
              -DMPI_CXX_LINK_FLAGS="-L$MPI_ROOT/lib"
              -DMPI_CXX_LIBRARIES="-lmpicxx -lmpi" \
              -DMPI_CXX_INCLUDE_PATH="$MPI_ROOT/include" \
