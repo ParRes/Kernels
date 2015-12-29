504c61100033050db051fa9c1ecfc9706d0e66ab
Jeff Hammond <jeff.science@gmail.com>
2015-12-29 09:28:29 -0800

    try to support GCC 4.8 w/ Grappa MPI+CMake

--- travis/install-grappa.sh
+++ travis/install-grappa.sh
@@ -17,6 +17,40 @@ if [ ! -d "$TRAVIS_ROOT/grappa" ]; then
 
         Linux)
             echo "Linux"
+            case "$CC" in
+                gcc)
+                    # I am sure there is a decent way to condense this...
+                    if [ -f "/usr/bin/gcc-5.3" ]; then
+                        export CMAKE_C_COMPILER=gcc-5.3
+                        export CMAKE_CXX_COMPILER=g++-5.3
+                    elif [ -f "/usr/bin/gcc-5.2" ]; then
+                        export CMAKE_C_COMPILER=gcc-5.2
+                        export CMAKE_CXX_COMPILER=g++-5.2
+                    elif [ -f "/usr/bin/gcc-5.1" ]; then
+                        export CMAKE_C_COMPILER=gcc-5.1
+                        export CMAKE_CXX_COMPILER=g++-5.1
+                    elif [ -f "/usr/bin/gcc-5" ]; then
+                        export CMAKE_C_COMPILER=gcc-5
+                        export CMAKE_CXX_COMPILER=g++-5
+                    elif [ -f "/usr/bin/gcc-4.9" ]; then
+                        export CMAKE_C_COMPILER=gcc-4.9
+                        export CMAKE_CXX_COMPILER=g++-4.9
+                    elif [ -f "/usr/bin/gcc-4.8" ]; then
+                        export CMAKE_C_COMPILER=gcc-4.8
+                        export CMAKE_CXX_COMPILER=g++-4.8
+                    elif [ -f "/usr/bin/gcc-4.7" ]; then
+                        export CMAKE_C_COMPILER=gcc-4.7
+                        export CMAKE_CXX_COMPILER=g++-4.7
+                    else
+                        export CMAKE_C_COMPILER=gcc
+                        export CMAKE_CXX_COMPILER=g++
+                    fi
+                    ;;
+                clang)
+                    export CMAKE_C_COMPILER=clang
+                    export CMAKE_CXX_COMPILER=clang++
+                    ;;
+            esac
             ;;
     esac
 
@@ -24,16 +58,11 @@ if [ ! -d "$TRAVIS_ROOT/grappa" ]; then
     git clone https://github.com/uwsampa/grappa.git grappa-source
     cd grappa-source
     # DEBUG
-    which gcc
-    #which gcc-4
-    #which gcc-4.6
-    #which gcc-4.7
-    #which gcc-4.8
     find /usr -name gcc\* -type f
     find $TRAVIS_ROOT/$MPI_IMPL
     # END
-    export MPI_C_COMPILER=$TRAVIS_ROOT/$MPI_IMPL/bin/mpicc
-    export MPI_CXX_COMPILER=$TRAVIS_ROOT/$MPI_IMPL/bin/mpicxx
+    export MPI_C_COMPILER="MPICH_CC=$CMAKE_CC_COMPILER $TRAVIS_ROOT/$MPI_IMPL/bin/mpicc"
+    export MPI_CXX_COMPILER="MPICH_CXX=$CMAKE_CXX_COMPILER $TRAVIS_ROOT/$MPI_IMPL/bin/mpicxx"
     export MPI_C_INCLUDE_PATH=$TRAVIS_ROOT/$MPI_IMPL/include
     export MPI_C_LIBRARIES=$TRAVIS_ROOT/$MPI_IMPL/lib/libmpi.so
     ./configure --prefix=$TRAVIS_ROOT/grappa
