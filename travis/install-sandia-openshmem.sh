6073c89dcbfe5bcb7bdbf5329acdbb6f273bd00b
Jeff Hammond <jeff.science@gmail.com>
2015-12-29 08:29:08 -0800

    factor out libfabric build from Sandia OpenSHMEM build

--- travis/install-sandia-openshmem.sh
+++ travis/install-sandia-openshmem.sh
@@ -5,15 +5,6 @@ TRAVIS_ROOT="$1"
 SHMEM_ROOT=$TRAVIS_ROOT/sandia-openshmem
 
 if [ ! -d "$SHMEM_ROOT" ]; then
-    # install OFI/libfabric
-    cd $TRAVIS_ROOT
-    git clone --depth 10 https://github.com/ofiwg/libfabric.git libfabric
-    cd libfabric
-    ./autogen.sh
-    ./configure --prefix=$SHMEM_ROOT
-    make && make install
-    export FI_LOG_LEVEL=error
-
     # install Hydra
     cd $TRAVIS_ROOT
     wget --no-check-certificate -q http://www.mpich.org/static/downloads/3.2/hydra-3.2.tar.gz
@@ -28,7 +19,7 @@ if [ ! -d "$SHMEM_ROOT" ]; then
     cd sandia-shmem
     ./autogen.sh
     # must build in-place (https://github.com/regrant/sandia-shmem/issues/49)
-    ./configure --with-ofi=$SHMEM_ROOT --with-ofi-libdir=$SHMEM_ROOT/lib \
+    ./configure --with-ofi=$TRAVIS_ROOT/libfabric --with-ofi-libdir=$TRAVIS_ROOT/libfabric/lib \
                 --disable-fortran --enable-error-checking \
                 --enable-remote-virtual-addressing --enable-pmi-simple \
                 --prefix=$SHMEM_ROOT
