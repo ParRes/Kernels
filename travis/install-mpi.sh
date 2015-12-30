305daea5109e055f90ad5db8a1658bc08eb00465
Jeff Hammond <jeff.science@gmail.com>
2015-12-29 20:53:36 -0800

    add OpenMPI build [ci skip]

--- travis/install-mpi.sh
+++ travis/install-mpi.sh
@@ -33,21 +33,34 @@ case "$os" in
             mpich)
                 if [ ! -d "$TRAVIS_ROOT/mpich" ]; then
                     wget --no-check-certificate -q http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
-                    tar xzf mpich-3.2.tar.gz
+                    tar -xzf mpich-3.2.tar.gz
                     cd mpich-3.2
                     mkdir build && cd build
                     ../configure --disable-fortran --prefix=$TRAVIS_ROOT/mpich
-                    make -j4 && make install
+                    make -j4
+                    make install
                 else
                     echo "MPICH installed..."
                     find $TRAVIS_ROOT/mpich -name mpiexec
                     find $TRAVIS_ROOT/mpich -name mpicc
-                    mpicc -show
                 fi
                 ;;
             openmpi)
-                echo "Need source build of Open-MPI since no-sudo"
-                exit 15
+                if [ ! -d "$TRAVIS_ROOT/openmpi" ]; then
+                    wget --no-check-certificate -q http://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-1.10.1.tar.bz2
+                    tar -xjf http://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-1.10.1.tar.bz2
+                    cd openmpi-1.10.1
+                    mkdir build && cd build
+                    ../configure --enable-mpi-fortran=none --prefix=$TRAVIS_ROOT/openmpi
+                    make -j4
+                    make install
+                else
+                    echo "OpenMPI installed..."
+                    find $TRAVIS_ROOT/openmpi -name mpiexec
+                    find $TRAVIS_ROOT/openmpi -name mpicc
+                fi
+
+
                 ;;
             *)
                 echo "Unknown MPI implementation: $MPI_IMPL"
