cbbffb5e61fcec3def454dfd59797e9a0df6c34e
Jeff Hammond <jeff.science@gmail.com>
2016-01-05 09:21:45 -0800

    disable smp with non-smp and udp with non-udp conduits

--- travis/install-berkeley-upc.sh
+++ travis/install-berkeley-upc.sh
@@ -26,22 +26,24 @@ if [ ! -d "$BUPC_PREFIX" ]; then
     case "$GASNET_CONDUIT" in
         smp)
             ../configure --prefix=$BUPC_PREFIX --disable-ibv --disable-aligned-segments \
-                         --enable-$GASNET_CONDUIT --without-mpi-cc
+                         --enable-$GASNET_CONDUIT --without-mpi-cc --disable-udp
             ;;
         udp)
             ../configure --prefix=$BUPC_PREFIX --disable-ibv --disable-aligned-segments \
-                         --enable-$GASNET_CONDUIT --without-mpi-cc
+                         --enable-$GASNET_CONDUIT --without-mpi-cc --disable-smp
             ;;
         ofi)
             # TODO factor Hydra out of Sandia OpenSHMEM install so it can be used as spawner here
             sh ./travis/install-libfabric.sh $TRAVIS_ROOT
             ../configure --prefix=$BUPC_PREFIX --disable-ibv --without-mpi-cc --disable-aligned-segments \
-                         --enable-$GASNET_CONDUIT --with-ofihome=$TRAVIS_ROOT/libfabric --with-ofi-spawner=ssh
+                         --enable-$GASNET_CONDUIT --with-ofihome=$TRAVIS_ROOT/libfabric --with-ofi-spawner=ssh \
+                         --disable-smp --disable-udp
             ;;
         mpi)
             sh ./travis/install-mpi.sh $TRAVIS_ROOT mpich
             ../configure --prefix=$BUPC_PREFIX --disable-ibv --disable-aligned-segments \
-                         --enable-$GASNET_CONDUIT --with-mpi-cc=$TRAVIS_ROOT/mpich/bin/mpicc
+                         --enable-$GASNET_CONDUIT --with-mpi-cc=$TRAVIS_ROOT/mpich/bin/mpicc \
+                         --disable-smp --disable-udp
             ;;
         *)
             echo "GASNet conduit not specified - configure will guess."
