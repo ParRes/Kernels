1367459b0224c9b2678cc8c2ecbcbb3398fa3558
Jeff Hammond <jeff.science@gmail.com>
2016-01-11 06:17:06 -0800

    only handle Mac+Clang case in clang script

--- travis/install-clang.sh
+++ travis/install-clang.sh
@@ -1,31 +1,31 @@
 set -e
 set -x
 
-os=`uname`
 TRAVIS_ROOT="$1"
 CLANG_VERSION="$2"
 
-case "$os" in
-    Darwin)
-        echo "Mac"
-        brew update
-        case "$CLANG_VERSION" in
-            omp)
-                brew install clang-omp
-                brew test clang-omp
-                ;;
-            37)
-                brew install llvm$CLANG_VERSION --with-clang --with-compiler-rt --with-libcxx --with-lld --without-assertions
-                brew test llvm$CLANG_VERSION
-                ;;
-            *)
-                echo "Unsupported version of Clang"
-                echo "Travis will continue and use the system default"
-                ;;
-        esac
+if [ ${CC} == "clang" || ${CXX} == "clang++" ]; then
+    os=`uname`
+    case "$os" in
+        Darwin)
+            echo "Mac"
+            brew update
+            case "$CLANG_VERSION" in
+                omp)
+                    brew install clang-omp
+                    brew test clang-omp
+                    ;;
+                37)
+                    brew install llvm$CLANG_VERSION --with-clang --with-compiler-rt --with-libcxx --with-lld --without-assertions
+                    brew test llvm$CLANG_VERSION
+                    ;;
+                *)
+                    echo "Unsupported version of Clang"
+                    echo "Travis will continue and use the system default"
+                    ;;
+            esac
+            ;;
+        Linux)
         ;;
-    Linux)
-        echo "Install Clang using APT on Linux"
-        exit 85
-    ;;
-esac
+    esac
+fi
