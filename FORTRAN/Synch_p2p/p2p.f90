07fe942bb0eb0cb3d110c53a62d48d5e31ccc9e9
Jeff Hammond <jeff.science@gmail.com>
2016-01-26 07:47:44 -0800

    synch_p2p, now in Fortran

--- FORTRAN/Synch_p2p/p2p.f90
+++ FORTRAN/Synch_p2p/p2p.f90
@@ -1,159 +1,173 @@
-/*
-Copyright (c) 2013, Intel Corporation
+!
+! Copyright (c) 2015, Intel Corporation
+!
+! Redistribution and use in source and binary forms, with or without
+! modification, are permitted provided that the following conditions
+! are met:
+!
+! * Redistributions of source code must retain the above copyright
+!       notice, this list of conditions and the following disclaimer.
+! * Redistributions in binary form must reproduce the above
+!       copyright notice, this list of conditions and the following
+!       disclaimer in the documentation and/or other materials provided
+!       with the distribution.
+! * Neither the name of Intel Corporation nor the names of its
+!       contributors may be used to endorse or promote products
+!       derived from this software without specific prior written
+!       permission.
+!
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+! COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+! POSSIBILITY OF SUCH DAMAGE.
 
-Redistribution and use in source and binary forms, with or without 
-modification, are permitted provided that the following conditions 
-are met:
+!*******************************************************************
+! NAME:    Pipeline
+!
+! PURPOSE: This program tests the efficiency with which point-to-point
+!          synchronization can be carried out. It does so by executing
+!          a pipelined algorithm on an m*n grid. The first array dimension
+!          is distributed among the threads (stripwise decomposition).
+!
+! USAGE:   The program takes as input the
+!          dimensions of the grid, and the number of iterations on the grid
+!
+!                <progname> <iterations> <m> <n>
+!
+!          The output consists of diagnostics to make sure the
+!          algorithm worked, and of timing statistics.
+!
+! FUNCTIONS CALLED:
+!
+!          Other than standard C functions, the following
+!          functions are used in this program:
+!
+! HISTORY: - Written by Rob Van der Wijngaart, February 2009.
+!            Converted to Fortran by Jeff Hammond, January 2015
+! *******************************************************************
 
-* Redistributions of source code must retain the above copyright 
-      notice, this list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above 
-      copyright notice, this list of conditions and the following 
-      disclaimer in the documentation and/or other materials provided 
-      with the distribution.
-* Neither the name of Intel Corporation nor the names of its 
-      contributors may be used to endorse or promote products 
-      derived from this software without specific prior written 
-      permission.
+program main
+  use iso_fortran_env
+  implicit none
+  ! for argument parsing
+  integer :: err
+  integer :: argnum, arglen
+  character(len=32) :: argtmp
+  ! problem definition
+  integer(kind=INT32) :: iterations                     ! number of times to run the pipeline algorithm
+  integer(kind=INT32) :: m, n
+  real(kind=REAL64) :: corner_val                       ! verification value at top right corner of grid
+  real(kind=REAL64), allocatable :: grid(:,:)           ! array holding grid values
+  ! runtime variables
+  integer(kind=INT32) ::  i, j, k
+  real(kind=REAL64) ::  t0, t1, pipeline_time, avgtime  ! timing parameters
+  real(kind=REAL64), parameter ::  epsilon=1.D-8        ! error tolerance
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
-FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
-COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
-LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
-ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-POSSIBILITY OF SUCH DAMAGE.
-*/
+  ! ********************************************************************
+  ! read and test input parameters
+  ! ********************************************************************
 
-/*******************************************************************
+  write(*,'(a,a)') 'Parallel Research Kernels version ', 'PRKVERSION'
+  write(*,'(a)')   'Serial pipeline execution on 2D grid'
 
-NAME:    Pipeline
+  if (command_argument_count().lt.3) then
+    write(*,'(a,i1)') 'argument count = ', command_argument_count()
+    write(*,'(a,a)')  'Usage: ./synch_p2p <# iterations> ',             &
+                      '<first array dimension> <second array dimension>'
+    stop 1
+  endif
 
-PURPOSE: This program tests the efficiency with which point-to-point
-         synchronization can be carried out. It does so by executing 
-         a pipelined algorithm on an m*n grid. The first array dimension
-         is distributed among the threads (stripwise decomposition).
-  
-USAGE:   The program takes as input the
-         dimensions of the grid, and the number of iterations on the grid
+  iterations = 1
+  call get_command_argument(1,argtmp,arglen,err)
+  if (err.eq.0) read(argtmp,'(i)') iterations
 
-               <progname> <iterations> <m> <n>
-  
-         The output consists of diagnostics to make sure the 
-         algorithm worked, and of timing statistics.
+  m = 1
+  call get_command_argument(2,argtmp,arglen,err)
+  if (err.eq.0) read(argtmp,'(i)') m
 
-FUNCTIONS CALLED:
+  n = 1
+  call get_command_argument(3,argtmp,arglen,err)
+  if (err.eq.0) read(argtmp,'(i)') n
 
-         Other than standard C functions, the following 
-         functions are used in this program:
+  if (iterations .lt. 1) then
+    write(*,'(a,i5)') 'ERROR: iterations must be >= 1 : ', iterations
+    stop 1
+  endif
 
-         wtime()
+  if ((m .lt. 1).or.(n .lt. 1)) then
+    write(*,'(a,i5,i5)') 'ERROR: array dimensions must be >= 1 : ', m, n
+    stop 1
+  endif
 
-HISTORY: - Written by Rob Van der Wijngaart, February 2009.
-*******************************************************************/
+  allocate( grid(m,n), stat=err)
+  if (err .ne. 0) then
+    write(*,'(a,i3)') 'allocation of grid returned ',err
+    stop 1
+  endif
 
-#include <par-res-kern_general.h>
+  write(*,'(a,i,i)') 'Grid sizes               = ', m, n
+  write(*,'(a,i)')   'Number of iterations     = ', iterations
 
-/* define shorthand for indexing a multi-dimensional array                       */
-#define ARRAY(i,j) vector[i+(j)*(m)]
+  do j=1,n
+    do i=1,m
+      grid(i,j) = 0.0d0
+    enddo
+  enddo
+  do j=1,n
+    grid(1,j) = real(j-1,REAL64)
+  enddo
+  do i=1,m
+    grid(i,1) = real(i-1,REAL64)
+  enddo
 
-int main(int argc, char ** argv) {
+  do k=0,iterations
 
-  long   m, n;            /* grid dimensions                                     */
-  int    i, j, iter;      /* dummies                                             */
-  int    iterations;      /* number of times to run the pipeline algorithm       */
-  double pipeline_time,   /* timing parameters                                   */
-         avgtime; 
-  double epsilon = 1.e-8; /* error tolerance                                     */
-  double corner_val;      /* verification value at top right corner of grid      */
-  double *RESTRICT vector;/* array holding grid values                           */
-  long   total_length;    /* total required length to store grid values          */
+    !  start timer after a warmup iteration
+    if (k.eq.1) call cpu_time(t0)
 
-  /*******************************************************************************
-  ** process and test input parameters    
-  ********************************************************************************/
+    do j=2,n
+      do i=2,m
+        grid(i,j) = grid(i-1,j) + grid(i,j-1) - grid(i-1,j-1)
+      enddo
+    enddo
 
-  printf("Parallel Research Kernels version %s\n", PRKVERSION);
-  printf("Serial pipeline execution on 2D grid\n");
+    ! copy top right corner value to bottom left corner to create dependency; we
+    ! need a barrier to make sure the latest value is used. This also guarantees
+    ! that the flags for the next iteration (if any) are not getting clobbered
+    grid(1,1) = -grid(m,n)
 
-  if (argc != 4){
-    printf("Usage: %s <# iterations> <first array dimension> ", *argv);
-    printf("<second array dimension>\n");
-    return(EXIT_FAILURE);
-  }
+  enddo ! iterations
 
-  iterations  = atoi(*++argv); 
-  if (iterations < 1){
-    printf("ERROR: iterations must be >= 1 : %d \n",iterations);
-    exit(EXIT_FAILURE);
-  }
+  call cpu_time(t1)
+  pipeline_time = t1 - t0
 
-  m  = atol(*++argv);
-  n  = atol(*++argv);
+  ! ********************************************************************
+  ! ** Analyze and output results.
+  ! ********************************************************************
 
-  if (m < 1 || n < 1){
-    printf("ERROR: grid dimensions must be positive: %ld, %ld \n", m, n);
-    exit(EXIT_FAILURE);
-  }
+  ! verify correctness, using top right value
+  corner_val = real((iterations+1)*(n+m-2),REAL64);
+  if (abs(grid(m,n)-corner_val)/corner_val .gt. epsilon) then
+    write(*,'(a,f10.2,a,f10.2)') 'ERROR: checksum ',grid(m,n), &
+            ' does not match verification value ', corner_val
+    stop 1
+  endif
 
-  total_length = sizeof(double)*m*n;
-  vector = (double *) malloc(total_length);
-  if (!vector) {
-    printf("ERROR: Could not allocate space for array: %ld\n", total_length);
-    exit(EXIT_FAILURE);
-  }
+  write(*,'(a)') 'Solution validates'
+  avgtime = pipeline_time/iterations
+  write(*,'(a,f13.6,a,f10.6)') 'Rate (MFlop/s): ',1.e-6*2*real((m-1)*(n-1),INT64)/avgtime, &
+         ' Avg time (s): ', avgtime
 
-  printf("Grid sizes                = %ld, %ld\n", m, n);
-  printf("Number of iterations      = %d\n", iterations);
+  deallocate( grid )
 
-  /* clear the array                                                             */
-  for (j=0; j<n; j++) for (i=0; i<m; i++) ARRAY(i,j) = 0.0;
-  /* set boundary values (bottom and left side of grid                           */
-  for (j=0; j<n; j++) ARRAY(0,j) = (double) j;
-  for (i=0; i<m; i++) ARRAY(i,0) = (double) i;
+  stop
 
-  for (iter = 0; iter<=iterations; iter++){
-
-    /* start timer after a warmup iteration */
-    if (iter == 1) pipeline_time = wtime();
-
-    for (j=1; j<n; j++) for (i=1; i<m; i++) {
-        ARRAY(i,j) = ARRAY(i-1,j) + ARRAY(i,j-1) - ARRAY(i-1,j-1);
-    }
-
-    /* copy top right corner value to bottom left corner to create dependency; we
-       need a barrier to make sure the latest value is used. This also guarantees
-       that the flags for the next iteration (if any) are not getting clobbered  */
-    ARRAY(0,0) = -ARRAY(m-1,n-1);
-  }
-
-  pipeline_time = wtime() - pipeline_time;
-
-  /*******************************************************************************
-  ** Analyze and output results.
-  ********************************************************************************/
-
-  /* verify correctness, using top right value;                                  */
-  corner_val = (double)((iterations+1)*(n+m-2));
-  if (abs(ARRAY(m-1,n-1)-corner_val)/corner_val > epsilon) {
-    printf("ERROR: checksum %lf does not match verification value %lf\n",
-           ARRAY(m-1,n-1), corner_val);
-    exit(EXIT_FAILURE);
-  }
-
-#ifdef VERBOSE   
-  printf("Solution validates; verification value = %lf\n", corner_val);
-#else
-  printf("Solution validates\n");
-#endif
-  avgtime = pipeline_time/iterations;
-  printf("Rate (MFlops/s): %lf Avg time (s): %lf\n",
-         1.0E-06 * 2 * ((double)((m-1)*(n-1)))/avgtime, avgtime);
-
-  exit(EXIT_SUCCESS);
-}
+end program
