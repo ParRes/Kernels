f1cd7521e2d0d549902fa930e55a23e06fd01865
Jeff Hammond <jeff.science@gmail.com>
2016-02-10 09:11:14 -0800

    switch SOS repo location

--- travis/install-sandia-openshmem.sh
+++ travis/install-sandia-openshmem.sh
@@ -5,7 +5,8 @@ TRAVIS_ROOT="$1"
 SHMEM_ROOT=$TRAVIS_ROOT/sandia-openshmem
 
 if [ ! -d "$SHMEM_ROOT" ]; then
-    git clone --depth 10 https://github.com/regrant/sandia-shmem.git
+    #git clone --depth 10 https://github.com/regrant/sandia-shmem.git
+    git clone --depth 10 https://github.com/Sandia-OpenSHMEM/SOS.git
     cd sandia-shmem
     ./autogen.sh
     # must build in-place (https://github.com/regrant/sandia-shmem/issues/49)
