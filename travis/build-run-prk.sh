d18a9a1f58c5335d55c746df3ed6ddc5142a269b
Jeff Hammond <jeff.science@gmail.com>
2016-02-19 07:36:27 -0800

    python in travis

--- travis/build-run-prk.sh
+++ travis/build-run-prk.sh
@@ -22,6 +22,13 @@ case "$os" in
 esac
 
 case "$PRK_TARGET" in
+    allpython)
+        echo "Python"
+        which python
+        python --version
+        export PRK_TARGET_PATH=PYTHON
+        python $PRK_TARGET_PATH/transpose.py 10 1024
+        ;;
     allserial)
         echo "Serial"
         echo "CC=$PRK_COMPILER" >> common/make.defs
