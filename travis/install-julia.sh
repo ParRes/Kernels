3b22d81ae99d8eb54405be233c7891b4711b0b75
Jeff Hammond <jeff.science@gmail.com>
2016-06-03 13:40:58 -0700

    debug julia linux install

--- travis/install-julia.sh
+++ travis/install-julia.sh
@@ -29,6 +29,8 @@ case "$os" in
             wget --no-check-certificate -q https://julialang.s3.amazonaws.com/bin/linux/x64/0.4/julia-0.4.5-linux-x86_64.tar.gz
             tar -C $TRAVIS_ROOT -xzvf julia-0.4.5-linux-x86_64.tar.gz
             ln -s $TRAVIS_ROOT/julia-2ac304dfba $TRAVIS_ROOT/julia
+            find $TRAVIS_ROOT -type f -name julia
+            find $TRAVIS_ROOT
         fi
         ;;
 esac
