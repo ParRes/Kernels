935d537b0955847778bc82b882651e1cf8436370
Jeff Hammond <jeff.science@gmail.com>
2016-06-03 12:57:35 -0700

    soft link julia direcory path

--- travis/install-julia.sh
+++ travis/install-julia.sh
@@ -27,8 +27,8 @@ case "$os" in
             mkdir -p $TRAVIS_ROOT/julia
             cd $TRAVIS_ROOT
             wget --no-check-certificate -q https://julialang.s3.amazonaws.com/bin/linux/x64/0.4/julia-0.4.5-linux-x86_64.tar.gz
-            tar -C $TRAVIS_ROOT/julia -xzvf julia-0.4.5-linux-x86_64.tar.gz
-            find $TRAVIS_ROOT -name julia
+            tar -C $TRAVIS_ROOT -xzvf julia-0.4.5-linux-x86_64.tar.gz
+            ln -s $TRAVIS_ROOT/julia-2ac304dfba $TRAVIS_ROOT/julia
         fi
         ;;
 esac
