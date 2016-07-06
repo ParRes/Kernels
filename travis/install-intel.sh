58470fe4acf2757c899714055b536cfa7771d937
Jeff Hammond <jeff.science@gmail.com>
2016-07-06 11:59:53 -0700

    first attempt at Intel tools

---
+++ travis/install-intel.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -x
+
+os=`uname`
+TRAVIS_ROOT="$1" # unused for now
+PRK_TARGET="$2"
+
+if [ "${CC}" = "icc" ] ; then
+    case "$os" in
+        Darwin)
+            echo "Intel tools in Mac Travis is on the TODO list..."
+            exit 5
+            ;;
+        Linux)
+            echo "Linux"
+            case "$PRK_TARGET" in
+                allserial)
+                    ./travis/install-icc.sh --components icc
+                    ;;
+                allfortran*)
+                    ./travis/install-icc.sh --components fortran,openmp
+                    ;;
+                allopenmp)
+                    ./travis/install-icc.sh --components icc,openmp
+                    ;;
+                allmpiopenmp)
+                    ./travis/install-icc.sh --components icc,mpi,openmp
+                    ;;
+                allmpi*)
+                    ./travis/install-icc.sh --components icc,mpi
+                    ;;
+                *)
+                    echo "Intel tools do not support PRK_TARGET=${PRK_TARGET}"
+                    exit 10
+                    ;;
+            esac
+            ;;
+    esac
+fi
