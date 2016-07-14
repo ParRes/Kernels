6b472e9460e9af0dc8f88ae3c3f53a521b26c79f
Jeff Hammond <jeff.science@gmail.com>
2016-07-14 13:05:19 -0700

    detect newer GCC and use for Chapel compiler build

--- travis/install-chapel.sh
+++ travis/install-chapel.sh
@@ -5,6 +5,34 @@ set -x
 
 TRAVIS_ROOT="$1"
 
+case "$CC" in
+    gcc)
+        for gccversion in "-6" "-5" "-5.3" "-5.2" "-5.1" "-4.9" "-4.8" "-4.7" "-4.6" "" ; do
+            if [ -f "`which gcc$gccversion`" ]; then
+                export PRK_CC="gcc$gccversion"
+                export PRK_CXX="g++$gccversion"
+                echo "Found GCC: $PRK_CC"
+                break
+            fi
+        done
+        ;;
+    clang)
+        for clangversion in "-3.9" "-3.8" "-3.7" "-3.6" "-3.5" "-3.4" "" ; do
+            find /usr/local -name clang$clangversion
+            if [ -f "`which clang$clangversion`" ]; then
+                export PRK_CC="clang$clangversion"
+                export PRK_CXX="clang++$clangversion"
+                echo "Found Clang: $PRK_CC"
+                break
+            fi
+        done
+        ;;
+    icc)
+        export PRK_CC=icc
+        export PRK_CXX=icpc
+        export PRK_FC=ifort
+esac
+
 if [ "${TRAVIS_OS_NAME}" = "osx" ] && [ "${CHPL_COMM}" = "none" ] ; then
     echo "Mac single-locale"
     brew update
@@ -22,7 +50,7 @@ else
     #ln -s chapel-1.12.0 chapel
     git clone --depth 10 https://github.com/chapel-lang/chapel.git
     cd chapel
-    make -j2
+    make CC=$PRK_CC CXX=$PRK_CXX -j2
     mkdir -p $TRAVIS_ROOT/bin
     ln -s `find $PWD -type f -name chpl` $TRAVIS_ROOT/bin/chpl
 fi
