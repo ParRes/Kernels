456a1b1cde2bf06aaef1968747177e10df1b77b2
Jeff Hammond <jeff.science@gmail.com>
2017-03-24 08:20:03 -0700

    make Jim's changes; remove Hydra build

--- travis/install-sandia-openshmem.sh
+++ travis/install-sandia-openshmem.sh
@@ -14,9 +14,8 @@ SHMEM_ROOT=$TRAVIS_ROOT/sandia-openshmem
 
 if [ ! -d "$SHMEM_ROOT" ]; then
     # master
-    #git clone --depth 10 https://github.com/regrant/sandia-shmem.git sandia-shmem
     #git clone --depth 10 https://github.com/Sandia-OpenSHMEM/SOS.git sandia-shmem
-    #git clone -b v1.3.0-rc1 --depth 10 https://github.com/Sandia-OpenSHMEM/SOS.git sandia-shmem
+    #git clone -b v1.3.2 --depth 10 https://github.com/Sandia-OpenSHMEM/SOS.git sandia-shmem
     #cd sandia-shmem
     # 1.3.2 release
     wget https://github.com/Sandia-OpenSHMEM/SOS/archive/v1.3.2.tar.gz
@@ -25,12 +24,13 @@ if [ ! -d "$SHMEM_ROOT" ]; then
     ./autogen.sh
     mkdir build
     cd build
-    ../configure --with-ofi=$TRAVIS_ROOT/libfabric --with-ofi-libdir=$TRAVIS_ROOT/libfabric/lib \
-                --disable-fortran \
-                --enable-error-checking \
-                --enable-remote-virtual-addressing \
-                --enable-pmi-simple --with-pmi=$TRAVIS_ROOT/hydra \
-                --prefix=$SHMEM_ROOT
+    ../configure --with-ofi=$TRAVIS_ROOT/libfabric \
+                 --with-libfabric=$TRAVIS_ROOT/libfabric \
+                 --disable-fortran \
+                 --enable-error-checking \
+                 --enable-remote-virtual-addressing \
+                 --enable-pmi-simple  \
+                 --prefix=$SHMEM_ROOT
     make && make install
 else
     echo "Sandia OpenSHMEM installed..."
