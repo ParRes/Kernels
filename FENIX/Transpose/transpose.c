d2d35384cfa1587f734da76e0863665579ae4c2a
rfvander <rob.f.van.der.wijngaart@intel.com>
2017-05-10 22:23:30 -0700

    Adding transpose to collection of Fenix-fortified kernels.

--- FENIX/Transpose/transpose.c
+++ FENIX/Transpose/transpose.c
@@ -119,8 +119,12 @@ o The original and transposed matrices are called A and B
 |           |           |           |                             |
  -----------------------------------------------------------------*/
 
+#include <signal.h>
+#include <sys/types.h>
 #include <par-res-kern_general.h>
-#include <par-res-kern_mpi.h>
+#include <par-res-kern_fenix.h>
+#include <random_draw.h>
+#include <unistd.h>
 
 #define A(i,j)        A_p[(i+istart)+order*(j)]
 #define B(i,j)        B_p[(i+istart)+order*(j)]
@@ -146,7 +150,7 @@ int main(int argc, char ** argv)
   int root=0;              /* rank of root                          */
   int iterations;          /* number of times to do the transpose   */
   int i, j, it, jt, istart;/* dummies                               */
-  int iter;                /* index of iteration                    */
+  int iter, iter_init;     /* index of iteration                    */
   int phase;               /* phase inside staged communication     */
   int colstart;            /* starting column for owning rank       */
   int error;               /* error flag                            */
@@ -157,9 +161,21 @@ int main(int argc, char ** argv)
   double abserr,           /* absolute error                        */
          abserr_tot;       /* aggregate absolute error              */
   double epsilon = 1.e-8;  /* error tolerance                       */
-  double local_trans_time, /* timing parameters                     */
-         trans_time,
+  double transpose_time,       /* timing parameters                     */
          avgtime;
+  int    spare_ranks;     /* number of ranks to keep in reserve                  */
+  int    kill_ranks;      /* number of ranks that die with each failure          */
+  int    *kill_set;       /* instance of set of ranks to be killed               */
+  int    kill_period;     /* average number of iterations between failures       */
+  int    *fail_iter;      /* list of iterations when a failure will be triggered */
+  int    fail_iter_s=0;   /* latest  */
+  double init_add, addit; /* used to offset initial solutions                    */
+  int    checkpointing;   /* indicates if data is restored using Fenix or
+                             analytically                                        */
+  int    num_fenix_init=1;/* number of times Fenix_Init is called                */
+  int    num_fenix_init_loc;/* number of times Fenix_Init was called             */
+  int    fenix_status;
+  random_draw_t dice;
 
 /*********************************************************************
 ** Initialize the MPI environment
@@ -174,55 +190,154 @@ int main(int argc, char ** argv)
   error = 0;
   if (my_ID == root) {
     printf("Parallel Research Kernels version %s\n", PRKVERSION);
-    printf("MPI matrix transpose: B = A^T\n");
+    printf("MPI matrix transpose with Fenix fault tolerance: B = A^T\n");
 
-    if (argc != 3 && argc != 4){
-      printf("Usage: %s <# iterations> <matrix order> [Tile size]\n",
+    if (argc != 7 && argc != 8){
+      printf("Usage: %s <# iterations> <matrix order> <spare ranks> ",
+                                                               *argv);
+      printf("<kill set size> <kill period> <checkpointing> [Tile size]\n",
                                                                *argv);
       error = 1; goto ENDOFTESTS;
     }
 
-    iterations  = atoi(*++argv);
+    iterations  = atoi(argv[1]);
     if(iterations < 1){
       printf("ERROR: iterations must be >= 1 : %d \n",iterations);
       error = 1; goto ENDOFTESTS;
     }
 
-    order = atol(*++argv);
-    if (order < Num_procs) {
+    order = atol(argv[2]);
+    spare_ranks  = atoi(argv[3]);
+    if (order < Num_procs-spare_ranks) {
       printf("ERROR: matrix order %ld should at least # procs %d\n",
-             order, Num_procs);
+             order, Num_procs-spare_ranks);
       error = 1; goto ENDOFTESTS;
     }
-    if (order%Num_procs) {
+    if (order%(Num_procs-spare_ranks)) {
       printf("ERROR: matrix order %ld should be divisible by # procs %d\n",
-             order, Num_procs);
+             order, Num_procs-spare_ranks);
       error = 1; goto ENDOFTESTS;
     }
 
-    if (argc == 4) Tile_order = atoi(*++argv);
+    if (spare_ranks < 0 || spare_ranks >= Num_procs){
+      printf("ERROR: Illegal number of spare ranks : %d \n", spare_ranks);
+      error = 1;
+      goto ENDOFTESTS;     
+    }
+
+    kill_ranks = atoi(argv[4]);
+    if (kill_ranks < 0 || kill_ranks > spare_ranks) {
+      printf("ERROR: Number of ranks in kill set invalid: %d\n", kill_ranks);
+      error = 1;
+      goto ENDOFTESTS;     
+    }
+
+    kill_period = atoi(argv[5]);
+    if (kill_period < 1) {
+      printf("ERROR: rank kill period must be positive: %d\n", kill_period);
+      error = 1;
+      goto ENDOFTESTS;     
+    }
+
+    checkpointing = atoi(argv[6]);
+    if (checkpointing) {
+      printf("ERROR: Fenix checkpointing not yet implemented\n");
+      error = 1;
+      goto ENDOFTESTS;     
+    }
+
+    if (argc == 8) Tile_order = atoi(argv[7]);
 
     ENDOFTESTS:;
   }
   bail_out(error);
 
+  /*  Broadcast input data to all ranks */
+  MPI_Bcast(&order,         1, MPI_LONG, root, MPI_COMM_WORLD);
+  MPI_Bcast(&iterations,    1, MPI_INT,  root, MPI_COMM_WORLD);
+  MPI_Bcast(&Tile_order,    1, MPI_INT,  root, MPI_COMM_WORLD);
+  MPI_Bcast(&spare_ranks,   1, MPI_INT, root, MPI_COMM_WORLD);
+  MPI_Bcast(&kill_ranks,    1, MPI_INT, root, MPI_COMM_WORLD);
+  MPI_Bcast(&kill_period,   1, MPI_INT, root, MPI_COMM_WORLD);
+  MPI_Bcast(&checkpointing, 1, MPI_INT, root, MPI_COMM_WORLD);
+
   if (my_ID == root) {
-    printf("Number of ranks      = %d\n", Num_procs);
-    printf("Matrix order         = %ld\n", order);
-    printf("Number of iterations = %d\n", iterations);
+    printf("Number of ranks       = %d\n", Num_procs);
+    printf("Matrix order          = %ld\n", order);
+    printf("Number of iterations  = %d\n", iterations);
     if ((Tile_order > 0) && (Tile_order < order))
-          printf("Tile size            = %d\n", Tile_order);
+          printf("Tile size             = %d\n", Tile_order);
     else  printf("Untiled\n");
 #if !SYNCHRONOUS
     printf("Non-");
 #endif
     printf("Blocking messages\n");
+    printf("Number of spare ranks = %d\n", spare_ranks);
+    printf("Kill set size         = %d\n", kill_ranks);
+    printf("Fault period          = %d\n", kill_period);
+    if (checkpointing)
+      printf("Data recovery         = Fenix checkpointing\n");
+    else
+      printf("Data recovery         = analytical\n");
   }
 
-  /*  Broadcast input data to all ranks */
-  MPI_Bcast(&order,      1, MPI_LONG, root, MPI_COMM_WORLD);
-  MPI_Bcast(&iterations, 1, MPI_INT,  root, MPI_COMM_WORLD);
-  MPI_Bcast(&Tile_order, 1, MPI_INT,  root, MPI_COMM_WORLD);
+  /* initialize the random number generator for each rank; we do that before
+     starting Fenix, so that all ranks, including spares, are initialized      */
+  LCG_init(&dice);
+  /* compute the iterations during which errors will be incurred               */
+  for (iter=0; iter<=iterations; iter++) {
+    fail_iter_s += random_draw(kill_period, &dice);
+    if (fail_iter_s > iterations) break;
+    num_fenix_init++;
+  }
+  if ((num_fenix_init-1)*kill_ranks>spare_ranks) {
+    if (my_ID==0) printf("ERROR: number of injected errors %d exceeds spare ranks %d\n",
+                         (num_fenix_init-1)*kill_ranks, spare_ranks);
+    error = 1;
+  }
+  else if(my_ID==root) printf("Total injected failures  = %d times %d errors\n", 
+                           num_fenix_init-1, kill_ranks);
+  bail_out(error);
+  if ((num_fenix_init-1)*kill_ranks>=Num_procs-spare_ranks) if (my_ID==root)
+  printf("WARNING: All active ranks will be replaced by recovered ranks; timings not valid\n");
+
+  fail_iter = (int *) prk_malloc(sizeof(int)*num_fenix_init);
+  if (!fail_iter) {
+    printf("ERROR: Rank %d could not allocate space for array fail_iter\n", my_ID);
+    error = 1;
+  }
+  bail_out(error);
+
+  /* reinitialize random number generator to obtain identical error series     */
+  LCG_init(&dice);
+  /* now record the actual failure iterations                                  */
+  for (fail_iter_s=iter=0; iter<num_fenix_init; iter++) {
+    fail_iter_s += random_draw(kill_period, &dice);
+    fail_iter[iter] = fail_iter_s;
+  }
+
+  /* Here is where we initialize Fenix and mark the return point after failure */
+  Fenix_Init(&fenix_status, MPI_COMM_WORLD, NULL, &argc, &argv, spare_ranks, 
+             0, MPI_INFO_NULL, &error);
+
+  if (error==FENIX_WARNING_SPARE_RANKS_DEPLETED) 
+    printf("ERROR: Rank %d: Cannot reconsitute original communicator\n", my_ID);
+  bail_out(error);
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_ID);
+  MPI_Comm_size(MPI_COMM_WORLD, &Num_procs);
+
+  /* if rank is recovered, set iter to a negative number, to be increased
+     to the actual value corresponding to the current iter value among
+     survivor ranks; handle number of Fenix_Init calls similarly               */
+  switch (fenix_status){
+    case FENIX_ROLE_INITIAL_RANK:   iter_init = num_fenix_init_loc = 0;    break;
+    case FENIX_ROLE_RECOVERED_RANK: iter_init = num_fenix_init_loc = iterations+1;   break;
+    case FENIX_ROLE_SURVIVOR_RANK:  iter_init = iter;  num_fenix_init_loc++;
+  }
+
+  MPI_Allreduce(&iter_init, &iter, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+  MPI_Allreduce(&num_fenix_init_loc, &num_fenix_init, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
 
   /* a non-positive tile size means no tiling of the local transpose */
   tiling = (Tile_order > 0) && (Tile_order < order);
@@ -243,44 +358,76 @@ int main(int argc, char ** argv)
 ** Create the column block of the test matrix, the row block of the
 ** transposed matrix, and workspace (workspace only if #procs>1)
 *********************************************************************/
-  A_p = (double *)prk_malloc(Colblock_size*sizeof(double));
-  if (A_p == NULL){
-    printf(" Error allocating space for original matrix on node %d\n",my_ID);
-    error = 1;
+  if (fenix_status != FENIX_ROLE_SURVIVOR_RANK) {
+    A_p = (double *)prk_malloc(Colblock_size*sizeof(double));
+    if (A_p == NULL){
+      printf(" Error allocating space for original matrix on node %d\n",my_ID);
+      error = 1;
+    }
+    bail_out(error);
+  }
+
+  if (fenix_status != FENIX_ROLE_SURVIVOR_RANK) {
+    B_p = (double *)prk_malloc(Colblock_size*sizeof(double));
+    if (B_p == NULL){
+      printf(" Error allocating space for transpose matrix on node %d\n",my_ID);
+      error = 1;
+    }
   }
   bail_out(error);
 
-  B_p = (double *)prk_malloc(Colblock_size*sizeof(double));
-  if (B_p == NULL){
-    printf(" Error allocating space for transpose matrix on node %d\n",my_ID);
-    error = 1;
-  }
-  bail_out(error);
-
-  if (Num_procs>1) {
+  if (fenix_status != FENIX_ROLE_SURVIVOR_RANK && Num_procs>1) {
     Work_in_p   = (double *)prk_malloc(2*Block_size*sizeof(double));
     if (Work_in_p == NULL){
       printf(" Error allocating space for work on node %d\n",my_ID);
       error = 1;
     }
-    bail_out(error);
     Work_out_p = Work_in_p + Block_size;
   }
+  bail_out(error);
 
   /* Fill the original column matrix                                                */
+  /* intialize the input and output arrays, note that if we use the analytical
+     solution to initialize, one might be tempted to skip this step for survivor
+     ranks, because they already have the correct (interim) values. That would
+     be wrong for two reasons: It is possible for ranks to be in different time
+     steps at the same time, and it is possible that error signal delivery to
+     a rank is delayed                                                         */
+  if (checkpointing) {
+    init_add = 0.0;
+    addit    = 0.0;
+  }
+  else {
+    init_add = (double) iter;
+    addit = ((double)(iter-1) * (double) (iter))/2.0;
+  }
   istart = 0;
   for (j=0;j<Block_order;j++)
     for (i=0;i<order; i++)  {
-      A(i,j) = (double) (order*(j+colstart) + i);
-      B(i,j) = 0.0;
+      A(i,j) = (double) (order*(j+colstart) + i) + init_add;
+      B(i,j) = ((double) ((j+colstart) + order*i)*iter + addit);
   }
 
-  for (iter = 0; iter<=iterations; iter++){
+  for (; iter<=iterations; iter++){
 
     /* start timer after a warmup iteration                                        */
     if (iter == 1) {
       MPI_Barrier(MPI_COMM_WORLD);
-      local_trans_time = wtime();
+      transpose_time = wtime();
+    }
+
+    /* inject failure if appropriate                                                */
+    if (iter == fail_iter[num_fenix_init]) {
+      pid_t pid = getpid();
+      if (my_ID < kill_ranks) {
+#if VERBOSE
+        printf("Rank %d, pid %d commits suicide in iter %d\n", my_ID, pid, iter);
+#endif
+        kill(pid, SIGKILL);
+      }
+#if VERBOSE
+      else printf("Rank %d, pid %d is survivor rank in iter %d\n", my_ID, pid, iter);
+#endif
     }
 
     /* do the local transpose                                                     */
@@ -349,23 +496,23 @@ int main(int argc, char ** argv)
     }  /* end of phase loop  */
   } /* end of iterations */
 
-  local_trans_time = wtime() - local_trans_time;
-  MPI_Reduce(&local_trans_time, &trans_time, 1, MPI_DOUBLE, MPI_MAX, root,
-             MPI_COMM_WORLD);
+  MPI_Barrier(MPI_COMM_WORLD);
+  transpose_time = wtime() - transpose_time;;
 
   abserr = 0.0;
   istart = 0;
-  double addit = ((double)(iterations+1) * (double) (iterations))/2.0;
+  addit = ((double)(iterations+1) * (double) (iterations))/2.0;
   for (j=0;j<Block_order;j++) for (i=0;i<order; i++) {
       abserr += ABS(B(i,j) - (double)((order*i + j+colstart)*(iterations+1)+addit));
   }
 
+  root = Num_procs-1;
   MPI_Reduce(&abserr, &abserr_tot, 1, MPI_DOUBLE, MPI_SUM, root, MPI_COMM_WORLD);
 
   if (my_ID == root) {
     if (abserr_tot < epsilon) {
       printf("Solution validates\n");
-      avgtime = trans_time/(double)iterations;
+      avgtime = transpose_time/(double)iterations;
       printf("Rate (MB/s): %lf Avg time (s): %lf\n",1.0E-06*bytes/avgtime, avgtime);
 #if VERBOSE
       printf("Summed errors: %f \n", abserr);
@@ -379,6 +526,7 @@ int main(int argc, char ** argv)
 
   bail_out(error);
 
+  Fenix_Finalize();
   MPI_Finalize();
   exit(EXIT_SUCCESS);
 
