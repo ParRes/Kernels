2e77e62a3ebb2e40066a5470557b9ef0e67284e9
Jeff Hammond <jeff.science@gmail.com>
2017-06-01 08:01:42 -0700

    julia binary update

--- travis/install-julia.sh
+++ travis/install-julia.sh
@@ -24,12 +24,15 @@ case "$os" in
     ;;
 
     Linux)
-        JULIA_NAME=julia-2ac304dfba
         echo "Linux"
+        #JULIA_NAME=julia-2ac304dfba    # julia-0.4.5-linux-x86_64.tar.gz
+        JULIA_NAME=julia-f4c6c9d4bb     # julia-0.5.2-linux-x86_64.tar.gz
         if [ ! -d "$TRAVIS_ROOT/$JULIA_NAME" ]; then
             cd $TRAVIS_ROOT
-            wget --no-check-certificate -q https://julialang.s3.amazonaws.com/bin/linux/x64/0.4/julia-0.4.5-linux-x86_64.tar.gz
-            tar -C $TRAVIS_ROOT -xzvf julia-0.4.5-linux-x86_64.tar.gz
+            #wget --no-check-certificate -q https://julialang.s3.amazonaws.com/bin/linux/x64/0.4/julia-0.4.5-linux-x86_64.tar.gz
+            #tar -C $TRAVIS_ROOT -xzvf julia-0.4.5-linux-x86_64.tar.gz
+            wget --no-check-certificate -q https://julialang-s3.julialang.org/bin/linux/x64/0.5/julia-0.5.2-linux-x86_64.tar.gz
+            tar -C $TRAVIS_ROOT -xzvf julia-0.5.2-linux-x86_64.tar.gz
             # symbolic link was not working for reasons i cannot explain
             ln -s $TRAVIS_ROOT/$JULIA_NAME $TRAVIS_ROOT/julia
             find $TRAVIS_ROOT -type f -name julia
