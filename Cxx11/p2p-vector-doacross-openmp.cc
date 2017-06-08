fbed215afd2b5e32fb9041c5127ae665f3100555
Jeff Hammond <jeff.science@gmail.com>
2017-06-08 09:45:39 -0700

    doacross better but still has race condition

--- Cxx11/p2p-vector-doacross-openmp.cc
+++ Cxx11/p2p-vector-doacross-openmp.cc
@@ -171,15 +171,14 @@ int main(int argc, char* argv[])
           }
         }
       } else /* chunking */ {
+        const size_t nmc = (m/mc) + (m%mc!=0); // number of m chunks
+        const size_t nnc = (n/nc) + (n%nc!=0); // number of n chunks
         _Pragma("omp for collapse(2) ordered(2)")
-        for (auto i=1; i<m; i+=mc) {
-          for (auto j=1; j<n; j+=nc) {
-            //_Pragma("omp ordered depend(sink: i-1,j) depend(sink: i,j-1)")
-            //_Pragma("omp ordered depend(sink:(i-mc)*n+j) depend(sink:(i*n+(j-nc))) depend(sink:((i-mc)*n+(j-nc)))")
-            _Pragma("omp ordered depend(sink:i-mc,j) depend(sink:i,j-nc) depend(sink:i-mc,j-nc))")
-            //_Pragma("omp ordered depend(sink:i-mc,j) depend(sink:i,j-nc))")
-            sweep_tile(i, std::min(m,i+mc),
-                       j, std::min(n,j+nc),
+        for (auto ic=0; ic<nmc; ic++) {
+          for (auto jc=0; jc<nnc; jc++) {
+            _Pragma("omp ordered depend(sink: ic-1,jc) depend(sink: ic,jc-1)")
+            sweep_tile(1+ic*mc, std::min(m,(ic+1)*mc),
+                       1+jc*nc, std::min(n,(jc+1)*nc),
                        m, n, grid);
             _Pragma("omp ordered depend(source)")
           }
