3bbdd85dfa3e2a1355bac25db9805c2623940d79
Jeff Hammond <jeff.science@gmail.com>
2017-06-09 16:16:29 -0700

    add p2p Fortran/OpenMP using wavefront/data-parallelism
    - this only works for symmetric grids because i am bad at math.
    - the performance is worse than serial.  the only reason i created this
      is because it might work for OpenCL.

--- FORTRAN/p2p-dp.f90
+++ FORTRAN/p2p-dp.f90
@@ -89,15 +89,12 @@ program main
   character(len=32) :: argtmp
   ! problem definition
   integer(kind=INT32) :: iterations                     ! number of times to run the pipeline algorithm
-  integer(kind=INT32) :: m, n
+  integer(kind=INT32) :: n
   real(kind=REAL64) :: corner_val                       ! verification value at top right corner of grid
   real(kind=REAL64), allocatable :: grid(:,:)           ! array holding grid values
   ! runtime variables
   integer(kind=INT32) :: i, j, k
-  integer(kind=INT32) :: ic, mc                         ! ic = chunking index, mc = chunking dimension
-  integer(kind=INT32) :: jc, nc                         ! jc = chunking index, nc = chunking dimension
-  integer(kind=INT32) :: lic, ljc                       ! hold indexes of last block
-  integer ::  me, nt
+  integer(kind=INT32) :: x, y
   real(kind=REAL64) ::  t0, t1, pipeline_time, avgtime  ! timing parameters
   real(kind=REAL64), parameter ::  epsilon=1.D-8        ! error tolerance
 
@@ -106,12 +103,12 @@ program main
   ! ********************************************************************
 
   write(*,'(a40)') 'Parallel Research Kernels'
-  write(*,'(a40)') 'Fortran OpenMP TASKS pipeline execution on 2D grid'
+  write(*,'(a40)') 'Fortran OpenMP wavefront pipeline execution on 2D grid'
 
   if (command_argument_count().lt.2) then
     write(*,'(a20,i1)') 'argument count = ', command_argument_count()
     write(*,'(a35,a50)')  'Usage: ./synch_p2p <# iterations> ',  &
-                          '<array x-dimension> <array y-dimension>'
+                          '<grid dimension>'
     stop 1
   endif
 
@@ -119,65 +116,38 @@ program main
   call get_command_argument(1,argtmp,arglen,err)
   if (err.eq.0) read(argtmp,'(i32)') iterations
 
-  m = 1
+  n = 1
   call get_command_argument(2,argtmp,arglen,err)
-  if (err.eq.0) read(argtmp,'(i32)') m
-
-  n = m
-  if (command_argument_count().gt.2) then
-    call get_command_argument(3,argtmp,arglen,err)
-    if (err.eq.0) read(argtmp,'(i32)') n
-
-    mc = m
-    call get_command_argument(4,argtmp,arglen,err)
-    if (err.eq.0) read(argtmp,'(i32)') mc
-
-    nc = n
-    call get_command_argument(5,argtmp,arglen,err)
-    if (err.eq.0) read(argtmp,'(i32)') nc
-  endif
+  if (err.eq.0) read(argtmp,'(i32)') n
 
   if (iterations .lt. 1) then
     write(*,'(a,i5)') 'ERROR: iterations must be >= 1 : ', iterations
     stop 1
   endif
 
-  if ((m .lt. 1).or.(n .lt. 1)) then
-    write(*,'(a,i5,i5)') 'ERROR: array dimensions must be >= 1 : ', m, n
+  if (n .lt. 1) then
+    write(*,'(a,i5,i5)') 'ERROR: array dimensions must be >= 1 : ', n
     stop 1
   endif
 
-  ! mc=m or nc=n disables chunking in that dimension, which means
-  ! there is no task parallelism to exploit
-  if (((mc.lt.1).or.(mc.gt.m)).or.((nc.lt.1).or.(nc.gt.n))) then
-    mc = int(m/omp_get_max_threads())
-    nc = int(n/omp_get_max_threads())
-  endif
-  mc = max(1,mc)
-  nc = max(1,nc)
-
   write(*,'(a,i8)')    'Number of threads        = ', omp_get_max_threads()
   write(*,'(a,i8)')    'Number of iterations     = ', iterations
-  write(*,'(a,i8,i8)') 'Grid sizes               = ', m, n
-  write(*,'(a,i8,i8)') 'Size of chunking         = ', mc, nc
+  write(*,'(a,i8,i8)') 'Grid sizes               = ', n, n
 
-  allocate( grid(m,n), stat=err)
+  allocate( grid(n,n), stat=err)
   if (err .ne. 0) then
     write(*,'(a,i3)') 'allocation of grid returned ',err
     stop 1
   endif
 
-  lic = (m/mc-1) * mc + 2
-  ljc = (n/nc-1) * nc + 2
-
   !$omp parallel default(none)                                  &
   !$omp&  shared(grid,t0,t1,iterations,pipeline_time)           &
-  !$omp&  firstprivate(m,n,mc,nc,ic,jc,lic,ljc)                 &
+  !$omp&  firstprivate(n)                 &
   !$omp&  private(i,j,k,corner_val)
 
   !$omp do collapse(2)
   do j=1,n
-    do i=1,m
+    do i=1,n
       grid(i,j) = 0.0d0
     enddo
   enddo
@@ -189,7 +159,7 @@ program main
   enddo
   !$omp end do
   !$omp do
-  do i=1,m
+  do i=1,n
     grid(i,1) = real(i-1,REAL64)
   enddo
   !$omp end do
@@ -204,17 +174,26 @@ program main
       !$omp end master
     endif
 
-    !$omp master
-    do ic=2,m,mc
-      do jc=2,n,nc
-        !$omp task depend(in:grid(1,1),grid(ic-mc,jc-nc),grid(ic-mc,jc),grid(ic,jc-nc)) depend(out:grid(ic,jc))
-        call sweep_tile(ic,min(m,ic+mc-1),jc,min(n,jc+nc-1),m,n,grid)
-        !$omp end task
+    do j=2,n
+      !$omp do
+      do i=2,j
+        x = i
+        y = j-i+2
+        grid(x,y) = grid(x-1,y) + grid(x,y-1) - grid(x-1,y-1)
       enddo
+      !$omp end do
     enddo
-    !$omp task depend(in:grid(lic,ljc)) depend(out:grid(1,1))
-    grid(1,1) = -grid(m,n)
-    !$omp end task
+    do j=n-1,2,-1
+      !$omp do
+      do i=2,j
+        x = n+i-j
+        y = n-i+2
+        grid(x,y) = grid(x-1,y) + grid(x,y-1) - grid(x-1,y-1)
+      enddo
+      !$omp end do
+    enddo
+    !$omp master
+    grid(1,1) = -grid(n,n)
     !$omp end master
 
   enddo ! iterations
@@ -232,16 +211,16 @@ program main
   ! ********************************************************************
 
   ! verify correctness, using top right value
-  corner_val = real((iterations+1)*(n+m-2),REAL64);
-  if (abs(grid(m,n)-corner_val)/corner_val .gt. epsilon) then
-    write(*,'(a,f10.2,a,f10.2)') 'ERROR: checksum ',grid(m,n), &
+  corner_val = real((iterations+1)*(2*n-2),REAL64);
+  if (abs(grid(n,n)-corner_val)/corner_val .gt. epsilon) then
+    write(*,'(a,f10.2,a,f10.2)') 'ERROR: checksum ',grid(n,n), &
             ' does not match verification value ', corner_val
     stop 1
   endif
 
   write(*,'(a)') 'Solution validates'
   avgtime = pipeline_time/iterations
-  write(*,'(a,f13.6,a,f10.6)') 'Rate (MFlop/s): ',2.d-6*real((m-1)*(n-1),INT64)/avgtime, &
+  write(*,'(a,f13.6,a,f10.6)') 'Rate (MFlop/s): ',2.d-6*real((n-1)*(n-1),REAL64)/avgtime, &
          ' Avg time (s): ', avgtime
 
   deallocate( grid )
