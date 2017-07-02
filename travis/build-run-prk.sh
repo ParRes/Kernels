04f3db066437c019b276ff89861fad901d8e7169
Jeff Hammond <jeff.science@gmail.com>
2017-07-01 17:15:14 -0700

    BUGFIX p2p args

--- travis/build-run-prk.sh
+++ travis/build-run-prk.sh
@@ -184,7 +184,7 @@ case "$PRK_TARGET" in
                 echo "OPENMPFLAG=-fopenmp" >> common/make.defs
                 make -C $PRK_TARGET_PATH p2p-tasks-openmp p2p-wavefront-openmp stencil-vector-openmp transpose-vector-openmp
                 $PRK_TARGET_PATH/p2p-tasks-openmp                 10 1024 1024 100 100
-                $PRK_TARGET_PATH/p2p-wavefront-openmp             10 1024
+                $PRK_TARGET_PATH/p2p-wavefront-openmp             10 1024 1024
                 $PRK_TARGET_PATH/stencil-vector-openmp            10 1000
                 $PRK_TARGET_PATH/transpose-vector-openmp          10 1024 32
                 #echo "Test stencil code generator"
@@ -219,7 +219,7 @@ case "$PRK_TARGET" in
                 echo "OPENMPFLAG=-qopenmp" >> common/make.defs
                 make -C $PRK_TARGET_PATH p2p-tasks-openmp p2p-wavefront-openmp stencil-vector-openmp transpose-vector-openmp
                 $PRK_TARGET_PATH/p2p-tasks-openmp                 10 1024 1024 100 100
-                $PRK_TARGET_PATH/p2p-wavefront-openmp             10 1024
+                $PRK_TARGET_PATH/p2p-wavefront-openmp             10 1024 1024
                 $PRK_TARGET_PATH/stencil-vector-openmp            10 1000
                 $PRK_TARGET_PATH/transpose-vector-openmp          10 1024 32
                 #echo "Test stencil code generator"
