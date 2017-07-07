1826841e5337ff2e80677e2c780488d388cac39c
Jeff Hammond <jeff.science@gmail.com>
2017-07-07 11:38:24 -0700

    more fixes to taskloop [ci skip]

--- C1z/generate-c-stencil.py
+++ C1z/generate-c-stencil.py
@@ -13,7 +13,7 @@ def codegen(src,pattern,stencil_size,radius,W,model):
         src.write('      PRAGMA_OMP_SIMD\n')
         src.write('      for (int j='+str(radius)+'; j<n-'+str(radius)+'; j++) {\n')
     elif (model=='taskloop'):
-        src.write('    _Pragma("omp taskloop")\n')
+        src.write('    _Pragma("omp taskloop firstprivate(n) shared(in,out)")\n')
         src.write('    for (int i='+str(radius)+'; i<n-'+str(radius)+'; i++) {\n')
         src.write('      PRAGMA_OMP_SIMD\n')
         src.write('      for (int j='+str(radius)+'; j<n-'+str(radius)+'; j++) {\n')
