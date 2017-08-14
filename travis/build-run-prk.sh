027be70f57806e2f9f280becb01657e1f9f5d6aa
Jeff Hammond <jeff.science@gmail.com>
2017-08-13 20:31:51 -0700

    test all RAJA transpose variants

--- travis/build-run-prk.sh
+++ travis/build-run-prk.sh
@@ -499,10 +499,18 @@ case "$PRK_TARGET" in
         make -C $PRK_TARGET_PATH stencil-vector-raja transpose-vector-raja
         $PRK_TARGET_PATH/stencil-vector-raja     10 1000
         # RAJA variant 11 should be the best
-        $PRK_TARGET_PATH/transpose-vector-raja   10 1024 11
+        $PRK_TARGET_PATH/transpose-vector-raja   10 1024
         # test all the RAJA variants with a smaller problem
-        for v in 1 2 3 4 5 6 7 10 11 12 13 14 15 ; do
-            $PRK_TARGET_PATH/transpose-vector-raja   10 200 $v
+        for f in seq omp tbb ; do
+         for s in y n ; do
+          for t in y n ; do
+           for n in y n ; do
+            for p in no ij ji ; do
+             $PRK_TARGET_PATH/transpose-vector-raja 4 200 nested=$n for=$f simd=$s tiled=$t permute=$p
+            done
+           done
+          done
+         done
         done
         for s in star grid ; do
             for r in 1 2 3 4 5 ; do
