370cb9c4df77e67eb020b6bfccbbbae99f9a221c
Jeff Hammond <jeff.science@gmail.com>
2017-08-16 20:46:27 -0700

    ISPC stencil

--- C1z/generate-c-stencil.py
+++ C1z/generate-c-stencil.py
@@ -7,40 +7,44 @@ import os
 
 def codegen(src,pattern,stencil_size,radius,W,model):
     if (model=='openmp'):
-        src.write('void '+pattern+str(radius)+'(const int n, const double * restrict in, double * restrict out) {\n')
+        src.write('void '+pattern+str(radius)+'(const int n, const double * restrict A, double * restrict B) {\n')
         src.write('    OMP_FOR()\n')
         src.write('    for (int i='+str(radius)+'; i<n-'+str(radius)+'; i++) {\n')
         src.write('      OMP_SIMD\n')
         src.write('      for (int j='+str(radius)+'; j<n-'+str(radius)+'; j++) {\n')
     elif (model=='target'):
-        src.write('void '+pattern+str(radius)+'(const int n, const double * restrict in, double * restrict out) {\n')
+        src.write('void '+pattern+str(radius)+'(const int n, const double * restrict A, double * restrict B) {\n')
         src.write('    OMP_TARGET( teams distribute parallel for simd collapse(2) schedule(static,1) )\n')
         src.write('    for (int i='+str(radius)+'; i<n-'+str(radius)+'; i++) {\n')
         src.write('      for (int j='+str(radius)+'; j<n-'+str(radius)+'; j++) {\n')
     elif (model=='taskloop'):
-        src.write('void '+pattern+str(radius)+'(const int n, const int gs, const double * restrict in, double * restrict out) {\n')
-        src.write('    OMP_TASKLOOP( firstprivate(n) shared(in,out) grainsize(gs) )\n')
+        src.write('void '+pattern+str(radius)+'(const int n, const int gs, const double * restrict A, double * restrict B) {\n')
+        src.write('    OMP_TASKLOOP( firstprivate(n) shared(A,B) grainsize(gs) )\n')
         src.write('    for (int i='+str(radius)+'; i<n-'+str(radius)+'; i++) {\n')
         src.write('      OMP_SIMD\n')
         src.write('      for (int j='+str(radius)+'; j<n-'+str(radius)+'; j++) {\n')
     elif (model=='cilk'):
-        src.write('void '+pattern+str(radius)+'(const int n, const double * restrict in, double * restrict out) {\n')
+        src.write('void '+pattern+str(radius)+'(const int n, const double * restrict A, double * restrict B) {\n')
         src.write('    _Cilk_for (int i='+str(radius)+'; i<n-'+str(radius)+'; i++) {\n')
         src.write('      PRAGMA_SIMD\n')
         src.write('      for (int j='+str(radius)+'; j<n-'+str(radius)+'; j++) {\n')
+    elif (model=='ispc'):
+        src.write('export void '+pattern+str(radius)+'(uniform const int n, uniform const double A[], uniform double B[]) {\n')
+        src.write('    for (uniform int i='+str(radius)+'; i<n-'+str(radius)+'; i++) {\n')
+        src.write('      for (uniform int j='+str(radius)+'; j<n-'+str(radius)+'; j++) {\n')
     else:
-        src.write('void '+pattern+str(radius)+'(const int n, const double * restrict in, double * restrict out) {\n')
+        src.write('void '+pattern+str(radius)+'(const int n, const double * restrict A, double * restrict B) {\n')
         src.write('    for (int i='+str(radius)+'; i<n-'+str(radius)+'; i++) {\n')
         src.write('      PRAGMA_SIMD\n')
         src.write('      for (int j='+str(radius)+'; j<n-'+str(radius)+'; j++) {\n')
-    src.write('        out[i*n+j] += ')
+    src.write('        B[i*n+j] += ')
     k = 0
     kmax = stencil_size-1;
     for j in range(0,2*radius+1):
         for i in range(0,2*radius+1):
             if ( W[j][i] != 0.0):
                 k+=1
-                src.write('+in[(i+'+str(j-radius)+')*n+(j+'+str(i-radius)+')] * '+str(W[j][i]))
+                src.write('+A[(i+'+str(j-radius)+')*n+(j+'+str(i-radius)+')] * '+str(W[j][i]))
                 if (k<kmax): src.write('\n')
                 if (k>0 and k<kmax): src.write('                      ')
     src.write(';\n')
@@ -74,8 +78,11 @@ def instance(src,model,pattern,r):
     codegen(src,pattern,stencil_size,r,W,model)
 
 def main():
-    for model in ['seq','openmp','target','cilk','taskloop']:
-      src = open('stencil_'+model+'.h','w')
+    for model in ['seq','openmp','target','cilk','taskloop','ispc']:
+      if (model=='ispc'):
+        src = open('stencil_'+model+'.ispc','w')
+      else:
+        src = open('stencil_'+model+'.h','w')
       if (model=='target'):
           src.write('OMP( declare target )\n')
       for pattern in ['star','grid']:
