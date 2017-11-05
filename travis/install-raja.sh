d34518418b121d8d17a3e52fa444e289ad939f42
Jeff Hammond <jeff.r.hammond@intel.com>
2017-11-05 15:39:22 -0800

    clone recursive due to BLT

--- travis/install-raja.sh
+++ travis/install-raja.sh
@@ -39,8 +39,7 @@ ${PRK_CXX} -v
 
 if [ ! -d "$TRAVIS_ROOT/raja" ]; then
     BRANCH=develop
-    #BRANCH=feature/trws/tbb-backend
-    git clone --depth 1 -b ${BRANCH} https://github.com/LLNL/RAJA.git
+    git clone --recursive --depth 1 -b ${BRANCH} https://github.com/LLNL/RAJA.git
     cd RAJA
     mkdir build
     cd build
