0461e861e78ab150ff59e000e725a09a07a8cb52
Jeff Hammond <jeff.r.hammond@intel.com>
2018-03-02 06:40:03 -0800

    sort of fix OpenCL p2p

--- Cxx11/p2p-innerloop-opencl.cc
+++ Cxx11/p2p-innerloop-opencl.cc
@@ -102,7 +102,7 @@ void run(cl::Context context, int iterations, int n)
     if (iter==1) pipeline_time = prk::wtime();
 
     cl::copy(queue,begin(h_grid), end(h_grid), d_grid);
-    kernel(cl::EnqueueArgs(queue, cl::NDRange(n,n)), n, d_grid);
+    kernel(cl::EnqueueArgs(queue, cl::NDRange(2*n)), n, d_grid);
     cl::copy(queue,d_grid, begin(h_grid), end(h_grid));
     queue.finish();
     h_grid[0*n+0] = -h_grid[(n-1)*n+(n-1)];
