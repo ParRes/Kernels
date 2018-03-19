ff02ca0d848d2c1ee7cc277b06d346414cff0c45
Jeff Hammond <jeff.science@gmail.com>
2018-03-19 08:51:51 -0700

    hoist code, use more compact syntax, etc (#322)
    hoist code, use more compact syntax, etc
    
    * fix compiler error with older gcc/clang
    ```
    In file included from stencil-sycl.cc:66:
    ./stencil_sycl.hpp:20:21: error: call to constructor of
    'cl::sycl::id<2>' is ambiguous
        cl::sycl::id<2> dx1({1,0});
    ```
    * move back to c++14 (and allow c++1z warning to persist)
    ```
    clang++-3.9 -std=c++1z -pthread  -DPRKVERSION="2.16" stencil-vector-raja.cc -DUSE_RAJA -I/home/travis/build/ParRes/Kernels/PRK-deps/raja/include -L/home/travis/build/ParRes/Kernels/PRK-deps/raja/lib -lRAJA  -o stencil-vector-raja
    In file included from stencil-vector-raja.cc:63:
    In file included from ./prk_util.h:217:
    In file included from /home/travis/build/ParRes/Kernels/PRK-deps/raja/include/RAJA/RAJA.hpp:45:
    In file included from /home/travis/build/ParRes/Kernels/PRK-deps/raja/include/RAJA/util/basic_mempool.hpp:38:
    In file included from /usr/bin/../lib/gcc/x86_64-linux-gnu/7.2.0/../../../../include/c++/7.2.0/map:60:
    In file included from /usr/bin/../lib/gcc/x86_64-linux-gnu/7.2.0/../../../../include/c++/7.2.0/bits/stl_tree.h:72:
    In file included from /usr/bin/../lib/gcc/x86_64-linux-gnu/7.2.0/../../../../include/c++/7.2.0/bits/node_handle.h:39:
    /usr/bin/../lib/gcc/x86_64-linux-gnu/7.2.0/../../../../include/c++/7.2.0/optional:1032:27:
    error: use of class template 'optional' requires template arguments
      template <typename _Tp> optional(_Tp) -> optional<_Tp>;
                              ^
    /usr/bin/../lib/gcc/x86_64-linux-gnu/7.2.0/../../../../include/c++/7.2.0/optional:451:11:
    note: template is declared here
        class optional
              ^
    /usr/bin/../lib/gcc/x86_64-linux-gnu/7.2.0/../../../../include/c++/7.2.0/optional:1032:40:
    error: expected ';' at end of declaration
      template <typename _Tp> optional(_Tp) -> optional<_Tp>;
                                           ^
    /usr/bin/../lib/gcc/x86_64-linux-gnu/7.2.0/../../../../include/c++/7.2.0/optional:1032:41:
    error: cannot use arrow operator on a type
      template <typename _Tp> optional(_Tp) -> optional<_Tp>;
                                            ^
    ```

--- Cxx11/stencil_sycl.hpp
+++ Cxx11/stencil_sycl.hpp
@@ -1,11 +1,9 @@
-void star1(cl::sycl::queue & q, const size_t n,
-           cl::sycl::buffer<double> & d_in,
-           cl::sycl::buffer<double> & d_out) {
+void star1(cl::sycl::queue & q, const size_t n, cl::sycl::buffer<double> & d_in, cl::sycl::buffer<double> & d_out)
+{
   q.submit([&](cl::sycl::handler& h) {
     auto in  = d_in.get_access<cl::sycl::access::mode::read>(h);
     auto out = d_out.get_access<cl::sycl::access::mode::read_write>(h);
-    h.parallel_for<class star1_1d>(cl::sycl::range<2> {n-2*1,n-2*1}, cl::sycl::id<2> {1,1},
-                                [=] (cl::sycl::item<2> it) {
+    h.parallel_for<class star1_1d>({n-2,n-2}, {1,1}, [=] (auto it) {
         out[it[0]*n+it[1]] += +in[it[0]*n+(it[1]+1)] * 0.5
                               +in[it[0]*n+(it[1]-1)] * -0.5
                               +in[(it[0]+1)*n+it[1]] * 0.5
@@ -14,17 +12,15 @@ void star1(cl::sycl::queue & q, const size_t n,
   });
 }
 
-void star1(cl::sycl::queue & q, const size_t n,
-           cl::sycl::buffer<double, 2> & d_in,
-           cl::sycl::buffer<double, 2> & d_out) {
+void star1(cl::sycl::queue & q, const size_t n, cl::sycl::buffer<double, 2> & d_in, cl::sycl::buffer<double, 2> & d_out)
+{
   q.submit([&](cl::sycl::handler& h) {
     auto in  = d_in.get_access<cl::sycl::access::mode::read>(h);
     auto out = d_out.get_access<cl::sycl::access::mode::read_write>(h);
-    h.parallel_for<class star1_2d>(cl::sycl::range<2> {n-2*1,n-2*1}, cl::sycl::id<2> {1,1},
-                                [=] (cl::sycl::item<2> it) {
+    cl::sycl::id<2> dx1(cl::sycl::range<2> {1,0});
+    cl::sycl::id<2> dy1(cl::sycl::range<2> {0,1});
+    h.parallel_for<class star1_2d>({n-2,n-2}, {1,1}, [=] (auto it) {
         cl::sycl::id<2> xy = it.get_id();
-        cl::sycl::id<2> dx1(cl::sycl::range<2> {1,0});
-        cl::sycl::id<2> dy1(cl::sycl::range<2> {0,1});
         out[xy] += +in[xy+dx1] * 0.5
                    +in[xy-dx1] * -0.5
                    +in[xy+dy1] * 0.5
@@ -33,14 +29,12 @@ void star1(cl::sycl::queue & q, const size_t n,
   });
 }
 
-void star2(cl::sycl::queue & q, const size_t n,
-           cl::sycl::buffer<double> & d_in,
-           cl::sycl::buffer<double> & d_out) {
+void star2(cl::sycl::queue & q, const size_t n, cl::sycl::buffer<double> & d_in, cl::sycl::buffer<double> & d_out)
+{
   q.submit([&](cl::sycl::handler& h) {
     auto in  = d_in.get_access<cl::sycl::access::mode::read>(h);
     auto out = d_out.get_access<cl::sycl::access::mode::read_write>(h);
-    h.parallel_for<class star2_1d>(cl::sycl::range<2> {n-2*2,n-2*2}, cl::sycl::id<2> {2,2},
-                                [=] (cl::sycl::item<2> it) {
+    h.parallel_for<class star2_1d>({n-4,n-4}, {2,2}, [=] (auto it) {
         out[it[0]*n+it[1]] += +in[it[0]*n+(it[1]+1)] * 0.25
                               +in[it[0]*n+(it[1]-1)] * -0.25
                               +in[(it[0]+1)*n+it[1]] * 0.25
@@ -53,19 +47,17 @@ void star2(cl::sycl::queue & q, const size_t n,
   });
 }
 
-void star2(cl::sycl::queue & q, const size_t n,
-           cl::sycl::buffer<double, 2> & d_in,
-           cl::sycl::buffer<double, 2> & d_out) {
+void star2(cl::sycl::queue & q, const size_t n, cl::sycl::buffer<double, 2> & d_in, cl::sycl::buffer<double, 2> & d_out)
+{
   q.submit([&](cl::sycl::handler& h) {
     auto in  = d_in.get_access<cl::sycl::access::mode::read>(h);
     auto out = d_out.get_access<cl::sycl::access::mode::read_write>(h);
-    h.parallel_for<class star2_2d>(cl::sycl::range<2> {n-2*2,n-2*2}, cl::sycl::id<2> {2,2},
-                                [=] (cl::sycl::item<2> it) {
+    cl::sycl::id<2> dx1(cl::sycl::range<2> {1,0});
+    cl::sycl::id<2> dy1(cl::sycl::range<2> {0,1});
+    cl::sycl::id<2> dx2(cl::sycl::range<2> {2,0});
+    cl::sycl::id<2> dy2(cl::sycl::range<2> {0,2});
+    h.parallel_for<class star2_2d>({n-4,n-4}, {2,2}, [=] (auto it) {
         cl::sycl::id<2> xy = it.get_id();
-        cl::sycl::id<2> dx1(cl::sycl::range<2> {1,0});
-        cl::sycl::id<2> dy1(cl::sycl::range<2> {0,1});
-        cl::sycl::id<2> dx2(cl::sycl::range<2> {2,0});
-        cl::sycl::id<2> dy2(cl::sycl::range<2> {0,2});
         out[xy] += +in[xy+dx1] * 0.25
                    +in[xy-dx1] * -0.25
                    +in[xy+dy1] * 0.25
@@ -78,14 +70,12 @@ void star2(cl::sycl::queue & q, const size_t n,
   });
 }
 
-void star3(cl::sycl::queue & q, const size_t n,
-           cl::sycl::buffer<double> & d_in,
-           cl::sycl::buffer<double> & d_out) {
+void star3(cl::sycl::queue & q, const size_t n, cl::sycl::buffer<double> & d_in, cl::sycl::buffer<double> & d_out)
+{
   q.submit([&](cl::sycl::handler& h) {
     auto in  = d_in.get_access<cl::sycl::access::mode::read>(h);
     auto out = d_out.get_access<cl::sycl::access::mode::read_write>(h);
-    h.parallel_for<class star3_1d>(cl::sycl::range<2> {n-2*3,n-2*3}, cl::sycl::id<2> {3,3},
-                                [=] (cl::sycl::item<2> it) {
+    h.parallel_for<class star3_1d>({n-6,n-6}, {3,3}, [=] (auto it) {
         out[it[0]*n+it[1]] += +in[it[0]*n+(it[1]+1)] * 0.166666666667
                               +in[it[0]*n+(it[1]-1)] * -0.166666666667
                               +in[(it[0]+1)*n+it[1]] * 0.166666666667
@@ -102,21 +92,19 @@ void star3(cl::sycl::queue & q, const size_t n,
   });
 }
 
-void star3(cl::sycl::queue & q, const size_t n,
-           cl::sycl::buffer<double, 2> & d_in,
-           cl::sycl::buffer<double, 2> & d_out) {
+void star3(cl::sycl::queue & q, const size_t n, cl::sycl::buffer<double, 2> & d_in, cl::sycl::buffer<double, 2> & d_out)
+{
   q.submit([&](cl::sycl::handler& h) {
     auto in  = d_in.get_access<cl::sycl::access::mode::read>(h);
     auto out = d_out.get_access<cl::sycl::access::mode::read_write>(h);
-    h.parallel_for<class star3_2d>(cl::sycl::range<2> {n-2*3,n-2*3}, cl::sycl::id<2> {3,3},
-                                [=] (cl::sycl::item<2> it) {
+    cl::sycl::id<2> dx1(cl::sycl::range<2> {1,0});
+    cl::sycl::id<2> dy1(cl::sycl::range<2> {0,1});
+    cl::sycl::id<2> dx2(cl::sycl::range<2> {2,0});
+    cl::sycl::id<2> dy2(cl::sycl::range<2> {0,2});
+    cl::sycl::id<2> dx3(cl::sycl::range<2> {3,0});
+    cl::sycl::id<2> dy3(cl::sycl::range<2> {0,3});
+    h.parallel_for<class star3_2d>({n-6,n-6}, {3,3}, [=] (auto it) {
         cl::sycl::id<2> xy = it.get_id();
-        cl::sycl::id<2> dx1(cl::sycl::range<2> {1,0});
-        cl::sycl::id<2> dy1(cl::sycl::range<2> {0,1});
-        cl::sycl::id<2> dx2(cl::sycl::range<2> {2,0});
-        cl::sycl::id<2> dy2(cl::sycl::range<2> {0,2});
-        cl::sycl::id<2> dx3(cl::sycl::range<2> {3,0});
-        cl::sycl::id<2> dy3(cl::sycl::range<2> {0,3});
         out[xy] += +in[xy+dx1] * 0.166666666667
                    +in[xy-dx1] * -0.166666666667
                    +in[xy+dy1] * 0.166666666667
@@ -133,14 +121,12 @@ void star3(cl::sycl::queue & q, const size_t n,
   });
 }
 
-void star4(cl::sycl::queue & q, const size_t n,
-           cl::sycl::buffer<double> & d_in,
-           cl::sycl::buffer<double> & d_out) {
+void star4(cl::sycl::queue & q, const size_t n, cl::sycl::buffer<double> & d_in, cl::sycl::buffer<double> & d_out)
+{
   q.submit([&](cl::sycl::handler& h) {
     auto in  = d_in.get_access<cl::sycl::access::mode::read>(h);
     auto out = d_out.get_access<cl::sycl::access::mode::read_write>(h);
-    h.parallel_for<class star4_1d>(cl::sycl::range<2> {n-2*4,n-2*4}, cl::sycl::id<2> {4,4},
-                                [=] (cl::sycl::item<2> it) {
+    h.parallel_for<class star4_1d>({n-8,n-8}, {4,4}, [=] (auto it) {
         out[it[0]*n+it[1]] += +in[it[0]*n+(it[1]+1)] * 0.125
                               +in[it[0]*n+(it[1]-1)] * -0.125
                               +in[(it[0]+1)*n+it[1]] * 0.125
@@ -161,23 +147,21 @@ void star4(cl::sycl::queue & q, const size_t n,
   });
 }
 
-void star4(cl::sycl::queue & q, const size_t n,
-           cl::sycl::buffer<double, 2> & d_in,
-           cl::sycl::buffer<double, 2> & d_out) {
+void star4(cl::sycl::queue & q, const size_t n, cl::sycl::buffer<double, 2> & d_in, cl::sycl::buffer<double, 2> & d_out)
+{
   q.submit([&](cl::sycl::handler& h) {
     auto in  = d_in.get_access<cl::sycl::access::mode::read>(h);
     auto out = d_out.get_access<cl::sycl::access::mode::read_write>(h);
-    h.parallel_for<class star4_2d>(cl::sycl::range<2> {n-2*4,n-2*4}, cl::sycl::id<2> {4,4},
-                                [=] (cl::sycl::item<2> it) {
+    cl::sycl::id<2> dx1(cl::sycl::range<2> {1,0});
+    cl::sycl::id<2> dy1(cl::sycl::range<2> {0,1});
+    cl::sycl::id<2> dx2(cl::sycl::range<2> {2,0});
+    cl::sycl::id<2> dy2(cl::sycl::range<2> {0,2});
+    cl::sycl::id<2> dx3(cl::sycl::range<2> {3,0});
+    cl::sycl::id<2> dy3(cl::sycl::range<2> {0,3});
+    cl::sycl::id<2> dx4(cl::sycl::range<2> {4,0});
+    cl::sycl::id<2> dy4(cl::sycl::range<2> {0,4});
+    h.parallel_for<class star4_2d>({n-8,n-8}, {4,4}, [=] (auto it) {
         cl::sycl::id<2> xy = it.get_id();
-        cl::sycl::id<2> dx1(cl::sycl::range<2> {1,0});
-        cl::sycl::id<2> dy1(cl::sycl::range<2> {0,1});
-        cl::sycl::id<2> dx2(cl::sycl::range<2> {2,0});
-        cl::sycl::id<2> dy2(cl::sycl::range<2> {0,2});
-        cl::sycl::id<2> dx3(cl::sycl::range<2> {3,0});
-        cl::sycl::id<2> dy3(cl::sycl::range<2> {0,3});
-        cl::sycl::id<2> dx4(cl::sycl::range<2> {4,0});
-        cl::sycl::id<2> dy4(cl::sycl::range<2> {0,4});
         out[xy] += +in[xy+dx1] * 0.125
                    +in[xy-dx1] * -0.125
                    +in[xy+dy1] * 0.125
@@ -198,14 +182,12 @@ void star4(cl::sycl::queue & q, const size_t n,
   });
 }
 
-void star5(cl::sycl::queue & q, const size_t n,
-           cl::sycl::buffer<double> & d_in,
-           cl::sycl::buffer<double> & d_out) {
+void star5(cl::sycl::queue & q, const size_t n, cl::sycl::buffer<double> & d_in, cl::sycl::buffer<double> & d_out)
+{
   q.submit([&](cl::sycl::handler& h) {
     auto in  = d_in.get_access<cl::sycl::access::mode::read>(h);
     auto out = d_out.get_access<cl::sycl::access::mode::read_write>(h);
-    h.parallel_for<class star5_1d>(cl::sycl::range<2> {n-2*5,n-2*5}, cl::sycl::id<2> {5,5},
-                                [=] (cl::sycl::item<2> it) {
+    h.parallel_for<class star5_1d>({n-10,n-10}, {5,5}, [=] (auto it) {
         out[it[0]*n+it[1]] += +in[it[0]*n+(it[1]+1)] * 0.1
                               +in[it[0]*n+(it[1]-1)] * -0.1
                               +in[(it[0]+1)*n+it[1]] * 0.1
@@ -230,25 +212,23 @@ void star5(cl::sycl::queue & q, const size_t n,
   });
 }
 
-void star5(cl::sycl::queue & q, const size_t n,
-           cl::sycl::buffer<double, 2> & d_in,
-           cl::sycl::buffer<double, 2> & d_out) {
+void star5(cl::sycl::queue & q, const size_t n, cl::sycl::buffer<double, 2> & d_in, cl::sycl::buffer<double, 2> & d_out)
+{
   q.submit([&](cl::sycl::handler& h) {
     auto in  = d_in.get_access<cl::sycl::access::mode::read>(h);
     auto out = d_out.get_access<cl::sycl::access::mode::read_write>(h);
-    h.parallel_for<class star5_2d>(cl::sycl::range<2> {n-2*5,n-2*5}, cl::sycl::id<2> {5,5},
-                                [=] (cl::sycl::item<2> it) {
+    cl::sycl::id<2> dx1(cl::sycl::range<2> {1,0});
+    cl::sycl::id<2> dy1(cl::sycl::range<2> {0,1});
+    cl::sycl::id<2> dx2(cl::sycl::range<2> {2,0});
+    cl::sycl::id<2> dy2(cl::sycl::range<2> {0,2});
+    cl::sycl::id<2> dx3(cl::sycl::range<2> {3,0});
+    cl::sycl::id<2> dy3(cl::sycl::range<2> {0,3});
+    cl::sycl::id<2> dx4(cl::sycl::range<2> {4,0});
+    cl::sycl::id<2> dy4(cl::sycl::range<2> {0,4});
+    cl::sycl::id<2> dx5(cl::sycl::range<2> {5,0});
+    cl::sycl::id<2> dy5(cl::sycl::range<2> {0,5});
+    h.parallel_for<class star5_2d>({n-10,n-10}, {5,5}, [=] (auto it) {
         cl::sycl::id<2> xy = it.get_id();
-        cl::sycl::id<2> dx1(cl::sycl::range<2> {1,0});
-        cl::sycl::id<2> dy1(cl::sycl::range<2> {0,1});
-        cl::sycl::id<2> dx2(cl::sycl::range<2> {2,0});
-        cl::sycl::id<2> dy2(cl::sycl::range<2> {0,2});
-        cl::sycl::id<2> dx3(cl::sycl::range<2> {3,0});
-        cl::sycl::id<2> dy3(cl::sycl::range<2> {0,3});
-        cl::sycl::id<2> dx4(cl::sycl::range<2> {4,0});
-        cl::sycl::id<2> dy4(cl::sycl::range<2> {0,4});
-        cl::sycl::id<2> dx5(cl::sycl::range<2> {5,0});
-        cl::sycl::id<2> dy5(cl::sycl::range<2> {0,5});
         out[xy] += +in[xy+dx1] * 0.1
                    +in[xy-dx1] * -0.1
                    +in[xy+dy1] * 0.1
