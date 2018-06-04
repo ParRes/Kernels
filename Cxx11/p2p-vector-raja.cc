5a83afc1ca634939fbba8f56ee52aeee7b394f58
Jeff Hammond <jeff.r.hammond@intel.com>
2018-06-04 11:02:37 -0500

    make work sans OpenMP

--- Cxx11/p2p-vector-raja.cc
+++ Cxx11/p2p-vector-raja.cc
@@ -150,14 +150,14 @@ int main(int argc, char* argv[])
     });
 #else
     for (auto j=1; j<n; j++) {
-      RAJA::forall<RAJA::omp_parallel_for_exec>(RAJA::Index_type(1), RAJA::Index_type(j+1), [&](RAJA::Index_type i) {
+      RAJA::forall<thread_exec>(RAJA::Index_type(1), RAJA::Index_type(j+1), [&](RAJA::Index_type i) {
         auto x = i;
         auto y = j-i+1;
         grid[x*n+y] = grid[(x-1)*n+y] + grid[x*n+(y-1)] - grid[(x-1)*n+(y-1)];
       });
     }
     for (auto j=n-2; j>=1; j--) {
-      RAJA::forall<RAJA::omp_parallel_for_exec>(RAJA::Index_type(1), RAJA::Index_type(j+1), [&](RAJA::Index_type i) {
+      RAJA::forall<thread_exec>(RAJA::Index_type(1), RAJA::Index_type(j+1), [&](RAJA::Index_type i) {
         auto x = n+i-j-1;
         auto y = n-i;
         grid[x*n+y] = grid[(x-1)*n+y] + grid[x*n+(y-1)] - grid[(x-1)*n+(y-1)];
