915864b0b5e7753cde84f7581dc6e064aada7656
Jeff Hammond <jeff.r.hammond@intel.com>
2019-05-19 21:44:12 -0700

    use 17 not 1z

--- travis/build-run-prk.sh
+++ travis/build-run-prk.sh
@@ -670,9 +670,9 @@ case "$PRK_TARGET" in
             SYCLDIR=${TRAVIS_ROOT}/triSYCL
             if [ "${CC}" = "clang" ] ; then
                 # SYCL will compile without OpenMP
-                echo "SYCLCXX=${PRK_CXX} -pthread -O3 -std=c++1z" >> common/make.defs
+                echo "SYCLCXX=${PRK_CXX} -pthread -O3 -std=c++17" >> common/make.defs
             else
-                echo "SYCLCXX=${PRK_CXX} -fopenmp -O3 -std=c++1z" >> common/make.defs
+                echo "SYCLCXX=${PRK_CXX} -fopenmp -O3 -std=c++17" >> common/make.defs
             fi
             echo "SYCLFLAG=-DUSE_SYCL -I${SYCLDIR}/include -I${TRAVIS_ROOT}/core/include" >> common/make.defs
             ${MAKE} -C $PRK_TARGET_PATH p2p-hyperplane-sycl stencil-sycl transpose-sycl nstream-sycl
