a106364e221ac93ae284c18ea7b9ac8f9cb3059e
Jeff Hammond <jeff.r.hammond@intel.com>
2019-09-30 14:30:53 -0700

    eliminate std::math in target region because not declare target (#415)
    also remove half-baked p2p

--- Cxx11/transpose-openmp-target.cc
+++ Cxx11/transpose-openmp-target.cc
@@ -132,8 +132,8 @@ int main(int argc, char * argv[])
         OMP_TARGET( teams distribute parallel for simd collapse(2) )
         for (int it=0; it<order; it+=tile_size) {
           for (int jt=0; jt<order; jt+=tile_size) {
-            for (int i=it; i<std::min(order,it+tile_size); i++) {
-              for (int j=jt; j<std::min(order,jt+tile_size); j++) {
+            for (int i=it; i<MIN(order,it+tile_size); i++) {
+              for (int j=jt; j<MIN(order,jt+tile_size); j++) {
                 B[i*order+j] += A[j*order+i];
                 A[j*order+i] += 1.0;
               }
