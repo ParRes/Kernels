b41ac65d2e23ada68b41b1f53e17ee6f385419f6
Jeff Hammond <jeff.r.hammond@intel.com>
2020-02-19 10:55:26 -0800

    improve parallel STL install

--- travis/install-pstl.sh
+++ travis/install-pstl.sh
@@ -3,6 +3,19 @@
 set -e
 set -x
 
+os=`uname`
 TRAVIS_ROOT="$1"
 
-git clone --depth 1 https://github.com/intel/parallelstl.git $TRAVIS_ROOT/pstl
+case "$os" in
+    Darwin)
+        echo "Mac"
+        brew update
+        brew upgrade parallelstl || brew install parallelstl
+        ;;
+    Linux)
+        echo "Linux"
+        if [ ! -d "$TRAVIS_ROOT/pstl" ]; then
+            git clone --depth 1 https://github.com/intel/parallelstl.git $TRAVIS_ROOT/pstl
+        fi
+        ;;
+esac
