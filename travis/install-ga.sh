0f3e32a12c83374fbe88143261fd5b87232fce8c
Jeff Hammond <jeff.r.hammond@intel.com>
2020-05-24 11:24:58 -0700

    Travis CI for GA uses ARMCI-MPI now
    Signed-off-by: Jeff Hammond <jeff.r.hammond@intel.com>

--- travis/install-ga.sh
+++ travis/install-ga.sh
@@ -12,7 +12,8 @@ if [ ! -d "$TRAVIS_ROOT/ga" ]; then
     mkdir build
     cd build
     #../configure CC=mpicc --prefix=$TRAVIS_ROOT/ga
-    ../configure --with-mpi3 MPICC=mpiicc MPICXX=mpiicpc MPIFC=mpiifort MPIF77=mpiifort --prefix=$TRAVIS_ROOT/ga && make -j8 install
+    #../configure --with-mpi3 MPICC=mpiicc MPICXX=mpiicpc MPIFC=mpiifort MPIF77=mpiifort --prefix=$TRAVIS_ROOT/ga && make -j8 install
+    ../configure --with-armci=${TRAVIS_ROOT}/armci-mpi MPICC=mpiicc MPICXX=mpiicpc MPIFC=mpiifort MPIF77=mpiifort --prefix=$TRAVIS_ROOT/ga && make -j8 install
     make
     make install
 else
