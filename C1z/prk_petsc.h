071482de0c52b3ddc6c510ae3c0415a56857567e
Jeff Hammond <jeff.r.hammond@intel.com>
2020-06-01 09:14:14 -0700

    add PETSc nstream, which works

---
+++ C1z/prk_petsc.h
@@ -0,0 +1,12 @@
+#ifndef PRK_PETSC_H_
+#define PRK_PETSC_H_
+
+#ifdef PRK_PETSC_USE_MPI
+#include <mpi.h>
+#endif
+
+#include <petsc.h>
+#include <petscsys.h>
+#include <petscvec.h>
+
+#endif // PRK_PETSC_H_
