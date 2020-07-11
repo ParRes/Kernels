da92c423e2070aaca7b574cc67ad7d78378c183e
Jeff Hammond <jeff.r.hammond@intel.com>
2020-07-10 18:08:44 -0700

    update copyright year

--- PYTHON/nstream-numpy-mpi.py
+++ PYTHON/nstream-numpy-mpi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2017, Intel Corporation
+# Copyright (c) 2020, Intel Corporation
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
