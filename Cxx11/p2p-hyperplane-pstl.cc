1aaacd9bf3fd18717524874e4b724dad3429bc2b
Jeff Hammond <jeff.r.hammond@intel.com>
2020-09-08 11:15:34 -0700

    use get_max_matrix_size utility

--- Cxx11/p2p-hyperplane-pstl.cc
+++ Cxx11/p2p-hyperplane-pstl.cc
@@ -89,7 +89,7 @@ int main(int argc, char* argv[])
       n = std::atoi(argv[2]);
       if (n < 1) {
         throw "ERROR: grid dimensions must be positive";
-      } else if ( static_cast<size_t>(n)*static_cast<size_t>(n) > static_cast<size_t>(INT_MAX)) {
+      } else if ( n > prk::get_max_matrix_size() ) {
         throw "ERROR: grid dimension too large - overflow risk";
       }
 
