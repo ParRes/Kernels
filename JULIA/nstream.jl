c9862205f5aef0866809c01c019b13498ec63542
Jeff Hammond <jeff.r.hammond@intel.com>
2020-11-06 22:07:30 -0800

    finish nstream

--- JULIA/nstream.jl
+++ JULIA/nstream.jl
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2015, Intel Corporation
+# Copyright (c) 2020, Intel Corporation
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -30,52 +30,68 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 #*******************************************************************
-#*******************************************************************
 #
-# NAME:    transpose
+# NAME:    nstream
 #
-# PURPOSE: This program measures the time for the transpose of a
-#          column-major stored matrix into a row-major stored matrix.
+# PURPOSE: To compute memory bandwidth when adding a vector of a given
+#          number of double precision values to the scalar multiple of
+#          another vector of the same length, and storing the result in
+#          a third vector.
 #
-# USAGE:   Program input is the matrix d and the number of times to
-#          repeat the operation:
+# USAGE:   The program takes as input the number
+#          of iterations to loop over the triad vectors, the length of the
+#          vectors, and the offset between vectors
 #
-#          transpose <% iterations> <matrix_size>
+#          <progname> <# iterations> <vector length> <offset>
 #
 #          The output consists of diagnostics to make sure the
-#          transpose worked and timing statistics.
+#          algorithm worked, and of timing statistics.
 #
-# HISTORY: Written by  Rob Van der Wijngaart, February 2009.
-#          Converted to Python by Jeff Hammond, February 2016.
-#          Converted to Julia by Jeff Hammond, June 2016.
-#          Converted to Octave by Jeff Hammond, May 2017.
+# NOTES:   Bandwidth is determined as the number of words read, plus the
+#          number of words written, times the size of the words, divided
+#          by the execution time. For a vector length of N, the total
+#          number of words read and written is 4*N*sizeof(double).
+#
+#
+# HISTORY: This code is loosely based on the Stream benchmark by John
+#          McCalpin, but does not follow all the Stream rules. Hence,
+#          reported results should not be associated with Stream in
+#          external publications
+#
+#          Converted to Python by Jeff Hammond, October 2017.
+#
 # *******************************************************************
+
 
 # ********************************************************************
 # read and test input parameters
 # ********************************************************************
 
-function do_initialize(A, B, C, length)
-    for i in 1:length
-        @inbounds A[i,j] = length * (j-1) + (i-1)
+function do_initialize(A, B, C, N)
+    for i in 1:N
+        @inbounds A[i] = 0.0
+        @inbounds B[i] = 2.0
+        @inbounds C[i] = 2.0
     end
 end
 
-function do_nstream(A, B, C, scalar, length)
-    for i in 1:length
-        @inbounds B[i,j] += A[j,i]
-        @inbounds A[j,i] += 1.0
+function do_nstream(A, B, C, s, N)
+    for i in 1:N
+        @inbounds A[i] += B[i] + s * C[i]
     end
 end
 
-function do_verify(B, length, iterations)
-    abserr = 0.0
-    return abserr
+function do_norm(A, N)
+    asum = 0.0
+    for i in 1:N
+        @inbounds asum += abs(A[i])
+    end
+    return asum
 end
 
 function main()
-    println("Parallel Research Kernels version ") #, PRKVERSION)
-    println("Julia Matrix transpose: B = A^T")
+    println("Parallel Research Kernels version")
+    println("Julia STREAM triad: A = B + scalar * C")
 
     if length(ARGS) != 2
         println("argument count = ", length(ARGS))
@@ -93,52 +109,65 @@ function main()
     end
 
     # vector length
-    length = argv[2]
-    if length < 1
+    vlength = argv[2]
+    if vlength < 1
         println("ERROR: length must be >= 1")
         exit(3)
     end
 
     println("Number of iterations     = ", iterations)
-    println("Vector length            = ", length);
+    println("Vector length            = ", vlength)
 
     # ********************************************************************
     # ** Allocate space for the input and transpose matrix
     # ********************************************************************
 
-    A = zeros(Float64,length)
-    B = zeros(Float64,length)
-    C = zeros(Float64,length)
-    precompile(do_initialize, (Array{Float64,2}, Int64))
-    do_initialize(A, length)
+    A = zeros(Float64,vlength)
+    B = zeros(Float64,vlength)
+    C = zeros(Float64,vlength)
+    precompile(do_initialize, (Array{Float64,1}, Int64))
+    do_initialize(A, B, C, vlength)
 
     # precompile hot function to smooth performance measurement
     precompile(do_nstream, (Array{Float64,1}, Array{Float64,1}, Array{Float64,1}, Float64, Int64))
 
+    scalar = 3.0
+
     t0 = time_ns()
 
-    for k in 1:iterations+1
-        do_nstream(A, B, C, scalar, length)
+    for k in 0:iterations
+        do_nstream(A, B, C, scalar, vlength)
     end
 
     t1 = time_ns()
-    trans_time = (t1 - t0) * 1.e-9
+    nstream_time = (t1 - t0) * 1.e-9
 
     # ********************************************************************
     # ** Analyze and output results.
     # ********************************************************************
 
-    precompile(do_verify, (Array{Float64,1}, Int64, Int64))
-    abserr = do_verify(A, length, iterations)
+    ar = 0.0
+    br = 2.0
+    cr = 2.0
+    for k in 0:iterations
+        ar += br + scalar * cr
+    end
 
-    epsilon=1.e-8
-    nbytes = 2 * length^2 * 8 # 8 is not sizeof(double) in bytes, but allows for comparison to C etc.
-    if abserr < epsilon
+    ar *= vlength
+
+    precompile(do_norm, (Array{Float64,1}, Int64))
+    asum = do_norm(A, vlength)
+
+    epsilon = 1.e-8
+    if abs(ar-asum)/asum < epsilon
         println("Solution validates")
-        avgtime = trans_time/iterations
+        avgtime = nstream_time/iterations
+        nbytes = 4.0 * vlength * 8 # 8 is not sizeof(double) in bytes, but allows for comparison to C etc.
         println("Rate (MB/s): ",1.e-6*nbytes/avgtime, " Avg time (s): ", avgtime)
     else
-        println("error ",abserr, " exceeds threshold ",epsilon)
+        println("Failed Validation on output array");
+        println("        Expected checksum: ",ar);
+        println("        Observed checksum: ",asum);
         println("ERROR: solution did not validate")
         exit(1)
     end
