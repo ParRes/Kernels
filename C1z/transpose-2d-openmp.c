2ad8ea34bcc23225c90f5b823aac64543840d4b3
Jeff Hammond <jeff.r.hammond@intel.com>
2021-01-08 13:07:24 -0800

    update copyright year

--- C1z/transpose-2d-openmp.c
+++ C1z/transpose-2d-openmp.c
@@ -1,5 +1,5 @@
 ///
-/// Copyright (c) 2013, Intel Corporation
+/// Copyright (c) 2020, Intel Corporation
 ///
 /// Redistribution and use in source and binary forms, with or without
 /// modification, are permitted provided that the following conditions
