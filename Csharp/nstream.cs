fd3073f53ddeec214c035e71e7a83b5ff7560d93
Jeff Hammond <jeff.r.hammond@intel.com>
2021-01-09 21:49:39 -0800

    finish nstream

--- Csharp/nstream.cs
+++ Csharp/nstream.cs
@@ -9,9 +9,14 @@ namespace PRK {
       Console.WriteLine("Usage: <# iterations> <vector length>");
     }
 
-    static void Main(string[] args) {
+    static void Main(string[] args)
+    {
       Console.WriteLine("Parallel Research Kernels");
       Console.WriteLine("Rust STREAM triad: A = B + scalar * C");
+
+      //////////////////////////////////////////////////////////////////////
+      // Read and test input parameters
+      //////////////////////////////////////////////////////////////////////
 
       if (args.Length != 2) {
           Help();
@@ -29,6 +34,11 @@ namespace PRK {
       } else {
           Help();
       }
+
+
+      //////////////////////////////////////////////////////////////////////
+      // Allocate space and perform the computation
+      //////////////////////////////////////////////////////////////////////
 
       double[] A = new double[length];
       double[] B = new double[length];
@@ -58,8 +68,39 @@ namespace PRK {
       timer.Stop();
       long tics = timer.ElapsedTicks;
       long freq = Stopwatch.Frequency;
-      double dt = (double)tics/(double)freq;
-      Console.WriteLine("tics={0} freq={1} dt={2}", tics, freq, dt);
+      double nstream_time = (double)tics/(double)freq;
+      //Console.WriteLine("tics={0} freq={1} dt={2}", tics, freq, nstream_time);
+
+      //////////////////////////////////////////////////////////////////////
+      // Analyze and output results
+      //////////////////////////////////////////////////////////////////////
+
+      double ar = 0.0;
+      double br = 2.0;
+      double cr = 2.0;
+      for (int k = 0 ; k <= iterations ; k++) {
+          ar += br + scalar * cr;
+      }
+
+      ar *= length;
+
+      double asum = 0.0;
+      for (int i = 0 ; i < length ; i++) {
+          asum += Math.Abs(A[i]);
+      }
+
+      const double epsilon=1e-8;
+      if (Math.Abs(ar-asum)/asum > epsilon) {
+          Console.WriteLine("Failed Validation on output array");
+          Console.WriteLine("       Expected checksum: {0}",ar);
+          Console.WriteLine("       Observed checksum: {0}",asum);
+          Console.WriteLine("ERROR: solution did not validate");
+      } else {
+          Console.WriteLine("Solution validates");
+          double avgtime = nstream_time/iterations;
+          double nbytes = 4.0 * length * sizeof(double);
+          Console.WriteLine("Rate (MB/s): {0} Avg time (s): {1}", 1e-6*nbytes/avgtime, avgtime);
+      }
 
     }
 
