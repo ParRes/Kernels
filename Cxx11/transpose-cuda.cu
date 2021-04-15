b6fa63e572377850e5db3e525753d0ed5385cba8
Jeff Hammond <jehammond@nvidia.com>
2021-04-15 16:23:44 -0700

    cleanup cuda

--- Cxx11/transpose-cuda.cu
+++ Cxx11/transpose-cuda.cu
@@ -147,7 +147,7 @@ int main(int argc, char * argv[])
         throw "ERROR: Matrix Order must be greater than 0";
       } else if (order > prk::get_max_matrix_size()) {
         throw "ERROR: matrix dimension too large - overflow risk";
-      } else if (order % TILE_DIM) {
+      } else if (order % tile_dim) {
         throw "ERROR: matrix dimension not divisible by tile size";
       }
 
