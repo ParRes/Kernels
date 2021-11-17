8dbdcf7bba8772de095d86f015228be9394d86b7
Jeff Hammond <jeff.science@gmail.com>
2021-11-17 17:21:40 +0200

    add transpose mpi w/ p2p

--- FORTRAN/transpose-p2p-mpi.F90
+++ FORTRAN/transpose-p2p-mpi.F90
@@ -100,14 +100,14 @@ program main
   real(kind=REAL64), allocatable ::  T(:,:)         ! temporary to hold tile
   real(kind=REAL64), parameter :: one=1.0d0
   ! runtime variables
-  integer(kind=INT64) ::  bytes
-  integer(kind=INT32) ::  i, j, k, r, lo, hi
-  !integer(kind=INT32) ::  it, jt, tile_size
+  integer(kind=INT64) :: bytes
+  integer(kind=INT32) :: i, j, k, lo, hi, phase
   real(kind=REAL64) ::  abserr, addit, temp
   real(kind=REAL64) ::  t0, t1, trans_time, avgtime
   real(kind=REAL64), parameter ::  epsilon=1.d-8
   ! MPI stuff
   integer(kind=INT32) :: me, np, provided
+  integer(kind=INT32) :: send_to, recv_from
 
   call MPI_Init_thread(MPI_THREAD_SINGLE,provided)
   call MPI_Comm_rank(MPI_COMM_WORLD, me)
@@ -158,7 +158,7 @@ program main
   ! ** Allocate space for the input and transpose matrix
   ! ********************************************************************
 
-  allocate( A(block_order,order), B(block_order,order), T(block_order,order), stat=err)
+  allocate( A(block_order,order), B(block_order,order), T(block_order,block_order), stat=err)
   if (err .ne. 0) then
     write(*,'(a,i3)') 'allocation  returned ',err
     stop 1
@@ -180,12 +180,23 @@ program main
     endif
 
     ! B += A^T
-    call MPI_Alltoall(A, block_order*block_order, MPI_DOUBLE_PRECISION, &
-                      T, block_order*block_order, MPI_DOUBLE_PRECISION, MPI_COMM_WORLD)
-    do r=0,np-1
-        lo = block_order * r + 1
-        hi = block_order * (r+1)
-        B(:,lo:hi) = B(:,lo:hi) + transpose(T(:,lo:hi))
+    !call MPI_Alltoall(A, block_order*block_order, MPI_DOUBLE_PRECISION, &
+    !                  T, block_order*block_order, MPI_DOUBLE_PRECISION, MPI_COMM_WORLD)
+    do phase=0,np-1
+        recv_from = mod( (me + phase     ), np)
+        send_to   = mod( (me - phase + np), np)
+
+        lo = block_order * send_to + 1
+        hi = block_order * (send_to+1)
+        call MPI_Sendrecv(A(:,lo:hi), block_order*block_order, MPI_DOUBLE_PRECISION,    &
+                          send_to,phase,                                                &
+                          T,block_order*block_order, MPI_DOUBLE_PRECISION,              &
+                          recv_from, phase, MPI_COMM_WORLD, MPI_STATUS_IGNORE)
+        lo = block_order * recv_from + 1
+        hi = block_order * (recv_from+1)
+        B(:,lo:hi) = B(:,lo:hi) + transpose(T)
+
+
     end do
     ! A += 1
     A = A + one
