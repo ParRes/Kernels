2a346a1a4cd8fb2f0db38a8b2d7c3716d48d2cae
Jeff Hammond <jeff.science@gmail.com>
2021-12-16 11:02:43 +0200

    new Fortran argument parser

--- FORTRAN/prk_mod.F90
+++ FORTRAN/prk_mod.F90
@@ -9,6 +9,192 @@ module prk
         t = real(c,REAL64) / real(r,REAL64)
       end function prk_get_wtime
 
+      subroutine prk_get_arguments(kernel,           & ! which kernel am i parsing?
+                                   iterations,       & ! everything
+                                   length, offset,   & ! nstream
+                                   order, tile_size, & ! transpose, stencil, dgemm
+                                   stencil, radius)    ! not supported in implementations yet
+        use iso_fortran_env
+        implicit none
+        character(len=*),    intent(in)  :: kernel
+        integer(kind=INT32), intent(out) :: iterations
+        integer(kind=INT64), intent(out), optional :: length, offset    ! nstream
+        integer(kind=INT32), intent(out), optional :: order, tile_size  ! transpose, stencil, dgemm
+        integer(kind=INT32), intent(out), optional :: radius            ! stencil
+        character(len=4),    intent(out), optional :: stencil           ! stencil
+
+        integer :: argc,arglen,err,a,p,q
+        character(len=64) :: argtmp
+
+        iterations = 10
+        if (present(length)) then
+          length = 1024*1024*32
+        endif
+        if (present(offset)) then
+          offset = 0
+        endif
+        if (present(order)) then
+          order = 1024
+        endif
+        if (present(tile_size)) then
+          tile_size = 32
+        endif
+        if (present(stencil)) then
+          stencil = 'star'
+        endif
+        if (present(radius)) then
+          radius = 2
+        endif
+
+#ifndef PRK_NO_ARGUMENTS
+        if (kernel(1:7).eq.'nstream') then
+          if (present(length)) then
+            length = 0
+          else
+            print*,'You cannot parse nstream arguments without length'
+            stop
+          endif
+        else if (    (kernel(1:9).eq.'transpose')     &
+                 .or.(kernel(1:7).eq.'stencil')       &
+                 .or.(kernel(1:5).eq.'dgemm') ) then
+          if (present(order)) then
+            order = 0
+          else
+            print*,'You cannot parse ',kernel,' arguments without order'
+            stop
+          endif
+        else
+          print*,kernel,'is not supported yet'
+          stop
+        endif
+
+        argc = command_argument_count()
+
+        if (argc.lt.2 ) then
+          write(*,'(a17,i2)') 'argument count = ', command_argument_count()
+          if (kernel(1:7).eq.'nstream') then
+            write(*,'(a62)') 'Old Usage: <program> <# iterations> <vector length> [<offset>]'
+            write(*,'(a87)') 'New Usage: <program> iterations=<# iterations> length=<vector length> [offset=<offset>]'
+          else if (    (kernel(1:9).eq.'transpose')     &
+                   .or.(kernel(1:7).eq.'stencil')       &
+                   .or.(kernel(1:5).eq.'dgemm') ) then
+            write(*,'(a53)') 'Old Usage: <program> <# iterations> <order> [<tile_size>]'
+            write(*,'(a73)') 'New Usage: <program> iterations=<# iterations> order=<order> [tile_size=<tile_size>]'
+          endif
+          STOP
+        endif
+
+        do a=1,argc
+          call get_command_argument(a,argtmp,arglen,err)
+          if (err.eq.0) then
+            p = index(argtmp,"=")
+            if (p.eq.0) then
+              if (a.eq.1) then
+                read(argtmp,'(i10)') iterations
+              else if (a.eq.2) then
+                if (present(length)) then
+                  read(argtmp,'(i15)') length
+                else if (present(order)) then
+                  read(argtmp,'(i7)') order
+                endif
+              else if (a.eq.3) then
+                if (present(offset)) then
+                  read(argtmp,'(i15)') offset
+                endif
+                if (present(tile_size)) then
+                  read(argtmp,'(i3)') tile_size
+                endif
+              else
+                print*,'too many positional arguments:',argc
+              endif
+            else ! found an =
+              ! look for iterations
+              q = index(argtmp(1:p-1),"it")
+              if (q.eq.1) then
+                read(argtmp(p+1:arglen),'(i10)') iterations
+              endif
+              ! look for length
+              if (present(length)) then
+                q = index(argtmp(1:p-1),"len")
+                if (q.eq.1) then
+                    read(argtmp(p+1:arglen),'(i15)') length
+                endif
+              endif
+              ! look for offset
+              if (present(offset)) then
+                q = index(argtmp(1:p-1),"off")
+                if (q.eq.1) then
+                  read(argtmp(p+1:arglen),'(i15)') offset
+                endif
+              endif
+              ! look for order
+              if (present(order)) then
+                q = index(argtmp(1:p-1),"ord")
+                if (q.eq.1) then
+                  read(argtmp(p+1:arglen),'(i7)') order
+                endif
+              endif
+              ! look for tile_size
+              if (present(tile_size)) then
+                q = index(argtmp(1:p-1),"tile")
+                if (q.eq.1) then
+                  read(argtmp(p+1:arglen),'(i3)') tile_size
+                endif
+              endif
+              ! look for radius
+              if (present(radius)) then
+                q = index(argtmp(1:p-1),"rad")
+                if (q.eq.1) then
+                  read(argtmp(p+1:arglen),'(i1)') radius
+                endif
+              endif
+            endif
+          endif
+        enddo
+
+        ! check all the relevant arguments for validity
+        if (iterations .lt. 1) then
+          write(*,'(a,i5)') 'ERROR: iterations must be positive : ', iterations
+          stop 1
+        endif
+
+        if (present(length)) then
+          if (length .lt. 1) then
+            write(*,'(a,i15)') 'ERROR: length must be positive : ', length
+            stop 1
+          endif
+        endif
+
+        if (present(order)) then
+          if (order .lt. 1) then
+            write(*,'(a,i7)') 'ERROR: order must be positive : ', order
+            stop 1
+          endif
+        endif
+
+        if (present(radius)) then
+          if (radius .lt. 1) then
+            write(*,'(a,i3)') 'ERROR: radius must be positive : ', radius
+            stop 1
+          endif
+        endif
+
+        if (present(offset)) then
+          if (offset .lt. 0) then
+            write(*,'(a,i15)') 'ERROR: offset must be nonnegative : ', offset
+            stop 1
+          endif
+        endif
+
+        if (present(tile_size)) then
+          if (tile_size .lt. 0) then
+            write(*,'(a,i3)') 'ERROR: tile_size must be nonnegative : ', tile_size
+            stop 1
+          endif
+        endif
+#endif
+      end subroutine
+
       subroutine initialize_w(is_star,r,W)
         use iso_fortran_env
         implicit none
