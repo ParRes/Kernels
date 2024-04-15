381b4c7c05c7fbd340965ac060fef728be3c4fef
Jeff Hammond <jehammond@nvidia.com>
2024-04-15 11:05:25 +0300

    print formatting
    Signed-off-by: Jeff Hammond <jehammond@nvidia.com>

--- Cxx11/dgemm-multigpu-cublas.cu
+++ Cxx11/dgemm-multigpu-cublas.cu
@@ -321,8 +321,6 @@ int main(int argc, char * argv[])
     std::cout << "Solution validates" << std::endl;
     auto avgtime = dgemm_time/iterations/matrices;
     auto nflops = 2.0 * prk::pow(forder,3) * ngpus;
-    //std::cout << "Rate (MF/s): " << 1.0e-6 * nflops/avgtime
-    //          << " Avg time (s): " << avgtime << std::endl;
     prk::print_flop_rate_time("FP64", nflops/avgtime, avgtime);
   } else {
     std::cout << "Reference checksum = " << reference << "\n"
