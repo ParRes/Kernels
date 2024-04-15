29830d75c5e96f3a7579f8b31e8c4cd7de8e68b4
Jeff Hammond <jeff.science@gmail.com>
2024-04-15 13:43:51 +0300

    fix VERBOSE; Apple still not working for SGEMM

--- Cxx11/sgemm-cblas.cc
+++ Cxx11/sgemm-cblas.cc
@@ -314,7 +314,7 @@ int main(int argc, char * argv[])
   if (residuum < epsilon) {
 #if VERBOSE
     std::cout << "Reference checksum = " << reference << "\n"
-              << "Actual checksum = " << checksum << std::endl;
+              << "Residuum           = " << residuum << std::endl;
 #endif
     std::cout << "Solution validates" << std::endl;
     auto avgtime = gemm_time/iterations/matrices;
@@ -324,10 +324,10 @@ int main(int argc, char * argv[])
     std::cout << "Reference checksum = " << reference << "\n"
               << "Residuum           = " << residuum << std::endl;
 #if VERBOSE
-    std::cout << "i, j, A, B, C, D" << std::endl;
+    std::cout << "i, j, A, B, C" << std::endl;
     for (int i=0; i<order; ++i)
       for (int j=0; j<order; ++j)
-        std::cout << i << "," << j << " = " << A[i*order+j] << ", " << B[i*order+j] << ", " << C[i*order+j] << ", " << D[i*order+j] << "\n";
+        std::cout << i << "," << j << " = " << A[0][i*order+j] << ", " << B[0][i*order+j] << ", " << C[0][i*order+j] << "\n";
     std::cout << std::endl;
 #endif
     return 1;
